9d054fcef975904cd578199289379fee2b4b0468
tonyrewin <anton.rewin@gmail.com>
2022-09-14 17:43:26 +0300

    private-routestore-fix

--- src/components/Nav/Private.tsx
+++ src/components/Nav/Private.tsx
@@ -1,21 +1,14 @@
-import { createSignal, onMount } from 'solid-js'
 import type { Author } from '../../graphql/types.gen'
 import Userpic from '../Author/Userpic'
 import Icon from './Icon'
 import './Private.scss'
-import { isServer } from 'solid-js/web'
 import { session as sesstore } from '../../stores/auth'
 import { useStore } from '@nanostores/solid'
+import { router } from '../../stores/router'
 
 export default () => {
   const session = useStore(sesstore)
-  const [resource, setResource] = createSignal('')
-
-  onMount(async () => {
-    if (!isServer) setResource(window.location.pathname)
-    console.log(`[private] mounting on ${resource()}`)
-  })
-
+  const routing = useStore(router)
   return (
     <div class="usercontrol col">
       <div class="usercontrol__item usercontrol__item--write-post">
@@ -31,14 +24,14 @@ export default () => {
       </div>
       <div class="usercontrol__item usercontrol__item--inbox">
         <a href="/inbox">
-          <div classList={{ entered: resource() === '/inbox' }}>
+          <div classList={{ entered: routing().path === '/inbox' }}>
             <Icon name="inbox-white" counter={session().info?.unread || 0} />
           </div>
         </a>
       </div>
       <div class="usercontrol__item">
         <a href={`/${session().user?.slug}`}>
-          <div classList={{ entered: resource() === `/${session().user?.slug}` }}>
+          <div classList={{ entered: routing().path === `/${session().user?.slug}` }}>
             <Userpic user={session().user as Author} />
           </div>
         </a>

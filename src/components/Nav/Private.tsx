cd803f1f07164e42291d2cbc7a08af68a94c7772
kvakazyambra <kvakazyambra@gmail.com>
2022-10-04 13:41:03 +0300

    Changed css classes to css-module in the Private component

--- src/components/Nav/Private.tsx
+++ src/components/Nav/Private.tsx
@@ -1,28 +1,29 @@
 import type { Author } from '../../graphql/types.gen'
 import Userpic from '../Author/Userpic'
 import { Icon } from './Icon'
-import './Private.module.scss'
+import styles from './Private.module.scss'
 import { useAuthStore } from '../../stores/auth'
 import { useRouter } from '../../stores/router'
+import clsx from 'clsx'
 
 export default () => {
   const { session } = useAuthStore()
   const { getPage } = useRouter()
 
   return (
-    <div class="userControl col">
-      <div class="userControlItem userControlItemWritePost">
+    <div class={clsx(styles.userControl, 'col')}>
+      <div class={clsx(styles.userControlItem, styles.userControlItemWritePost)}>
         <a href="/create">
-          <span class="text-label">опубликовать материал</span>
+          <span class={styles.textLabel}>опубликовать материал</span>
           <Icon name="pencil" />
         </a>
       </div>
-      <div class="userControlItem userControlItemSearch">
+      <div class={clsx(styles.userControlItem, styles.userControlItemSearch)}>
         <a href="/search">
           <Icon name="search" />
         </a>
       </div>
-      <div class="userControlItem userControlItemInbox">
+      <div class={clsx(styles.userControlItem, styles.userControlItemInbox)}>
         <a href="/inbox">
           {/*FIXME: replace with route*/}
           <div classList={{ entered: getPage().path === '/inbox' }}>
@@ -30,7 +31,7 @@ export default () => {
           </div>
         </a>
       </div>
-      <div class="userControlItem">
+      <div class={styles.userControlItem}>
         <a href={`/${session().user?.slug}`}>
           {/*FIXME: replace with route*/}
           <div classList={{ entered: getPage().path === `/${session().user?.slug}` }}>

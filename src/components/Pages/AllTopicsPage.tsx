70469dbe33808d7cafac66d17fc7a402ec44b1f6
Igor Lobanov <bniwredyc@gmail.com>
2022-10-09 12:56:39 +0200

    fixes, loading icon

--- src/components/Pages/AllTopicsPage.tsx
+++ src/components/Pages/AllTopicsPage.tsx
@@ -2,8 +2,8 @@ import { MainLayout } from '../Layouts/MainLayout'
 import { AllTopicsView } from '../Views/AllTopics'
 import type { PageProps } from '../types'
 import { createSignal, onMount, Show } from 'solid-js'
-import { t } from '../../utils/intl'
 import { loadAllTopics } from '../../stores/zine/topics'
+import { Loading } from '../Loading'
 
 export const AllTopicsPage = (props: PageProps) => {
   const [isLoaded, setIsLoaded] = createSignal<boolean>(Boolean(props.allTopics))
@@ -19,7 +19,7 @@ export const AllTopicsPage = (props: PageProps) => {
 
   return (
     <MainLayout>
-      <Show when={isLoaded()} fallback={t('Loading')}>
+      <Show when={isLoaded()} fallback={<Loading />}>
         <AllTopicsView topics={props.allTopics} />
       </Show>
     </MainLayout>

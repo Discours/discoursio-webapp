1d7a71ae3acb82bd9170983ea51be1e84ba0661f
tonyrewin <anton.rewin@gmail.com>
2022-10-19 19:35:24 +0300

    no reactions in p2p

--- src/components/Editor/prosemirror/p2p.ts
+++ src/components/Editor/prosemirror/p2p.ts
@@ -2,19 +2,17 @@ import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generato
 import { Awareness } from 'y-protocols/awareness'
 import { WebrtcProvider } from 'y-webrtc'
 import { Doc, XmlFragment } from 'yjs'
-import type { Reaction } from '../../../graphql/types.gen'
-import { setReactions } from '../../../stores/editor'
+// import type { Reaction } from '../../../graphql/types.gen'
+// import { setReactions } from '../../../stores/editor'
 
 export const roomConnect = (room: string, username = '', keyname = 'collab'): [XmlFragment, WebrtcProvider] => {
   const ydoc = new Doc()
-  const yarr = ydoc.getArray(keyname + '-reactions')
-
+  // const yarr = ydoc.getArray(keyname + '-reactions')
   // TODO: use reactions
-  yarr.observeDeep(() => {
-    console.debug('[p2p] yarray updated', yarr.toArray())
-    setReactions(yarr.toArray() as Reaction[])
-  })
-
+  // yarr.observeDeep(() => {
+  //  console.debug('[p2p] yarray updated', yarr.toArray())
+  //  setReactions(yarr.toArray() as Reaction[])
+  // })
   const yXmlFragment = ydoc.getXmlFragment(keyname)
   const webrtcOptions = {
     awareness: new Awareness(ydoc),

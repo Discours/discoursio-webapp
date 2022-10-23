70535f2366452700a891115609955ecf71054cd2
tonyrewin <anton.rewin@gmail.com>
2022-10-23 09:17:19 +0300

    fix-aws-crt

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,14 +1,14 @@
 import { createReadStream } from 'fs'
-// import { S3Client, type S3ClientConfig, PutObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, type S3ClientConfig, PutObjectCommand } from '@aws-sdk/client-s3'
 
 // const accessKeyId = process.env.ACCESS_KEY
 // const secretAccessKey = process.env.SECRET_KEY
 
-const config = {
+const config: S3ClientConfig = {
   apiVersion: '2006-03-01',
   region: 'eu-east-1'
 }
-// const s3 = new S3Client(config)
+const s3 = new S3Client(config)
 
 export default async function handler(req, res) {
   const params = {
@@ -16,6 +16,6 @@ export default async function handler(req, res) {
     Key: 'file-name', // FIXME
     Body: createReadStream('file-path') // FIXME
   }
-  // await s3.send(new PutObjectCommand(params))
+  await s3.send(new PutObjectCommand(params))
   res.status(200).json() // FIXME
 }

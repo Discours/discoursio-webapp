2142837d34f366f6159689a1ecbbd1a5afca8370
tonyrewin <anton.rewin@gmail.com>
2022-11-03 09:07:57 +0300

    fix-queries

--- src/graphql/query/im-chats.ts
+++ src/graphql/query/im-chats.ts
@@ -3,20 +3,24 @@ import { gql } from '@urql/core'
 export default gql`
   query GetChatsQuery {
     myChats {
-      messages {
-        id
-        author
-        body
-        replyTo
-        createdAt
+      error
+      chats {
+        title
+        description
+        updatedAt
+        messages {
+          id
+          author
+          body
+          replyTo
+          createdAt
+        }
+        users {
+          slug
+          name
+          userpic
+        }
       }
-      users {
-        slug
-        name
-        userpic
-      }
-      title
-      createdAt
     }
   }
 `
--- src/graphql/query/im-load-messages.ts
+++ src/graphql/query/im-load-messages.ts
@@ -3,11 +3,14 @@ import { gql } from '@urql/core'
 export default gql`
   query LoadMessagesQuery($chatId: String!, $offset: Int, $amount: Int) {
     loadChat(chatId: $chatId, offset: $offset, amount: $amount) {
-      author
-      body
-      createdAt
-      updatedAt
-      seen
+      error
+      messages {
+        author
+        body
+        createdAt
+        updatedAt
+        seen
+      }
     }
   }
 `

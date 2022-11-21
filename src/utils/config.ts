abe0b2cbaab58d9004b29091fbf597ce849a64b8
Igor Lobanov <igor.lobanov@onetwotrip.com>
2022-11-21 12:41:26 +0100

    test env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,5 @@ export const isDev = import.meta.env.MODE === 'development'
 
 const defaultApiUrl = 'https://v2.discours.io'
 export const apiBaseUrl = import.meta.env.PUBLIC_API_URL || defaultApiUrl
+
+console.log(import.meta.env)

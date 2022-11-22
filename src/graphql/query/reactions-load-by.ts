76460a1289bc722fc25384ab6f60499bb2139628
tonyrewin <anton.rewin@gmail.com>
2022-11-22 21:20:26 +0300

    working load

--- src/graphql/query/reactions-load-by.ts
+++ src/graphql/query/reactions-load-by.ts
@@ -1,32 +1,18 @@
 import { gql } from '@urql/core'
 
 export default gql`
-  query LoadReactionsByQuery($by: ReactionBy!, $limit: Int!, $offset: Int!) {
+  query LoadReactions($by: ReactionBy!, $limit: Int, $offset: Int) {
     loadReactionsBy(by: $by, limit: $limit, offset: $offset) {
       id
-      createdBy {
-        slug
-        name
-        userpic
-      }
-      body
-      kind
-      createdAt
-      updatedAt
       shout {
-        slug
         title
       }
-      replyTo {
-        id
-        createdBy {
-          slug
-          userpic
-          name
-        }
-        body
-        kind
+      body
+      createdAt
+      createdBy {
+        name
       }
+      updatedAt
     }
   }
 `

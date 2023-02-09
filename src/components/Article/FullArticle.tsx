3b7d669dadd6ec33628ce419112ed57aee786534
kvakazyambra <kvakazyambra@gmail.com>
2023-02-10 01:54:53 +0300

    Fixed slider on page gallery

--- src/components/Article/FullArticle.tsx
+++ src/components/Article/FullArticle.tsx
@@ -89,151 +89,160 @@ export const FullArticle = (props: ArticleProps) => {
   })
 
   return (
-    <div class="shout wide-container">
-      <article class="col-md-6 shift-content">
-        <div class={styles.shoutHeader}>
-          <div class={styles.shoutTopic}>
-            <a href={`/topic/${props.article.mainTopic}`} innerHTML={mainTopicTitle() || ''} />
-          </div>
+    <>
+      <div class="shout wide-container">
+        <article class="col-md-6 shift-content">
+          <div class={styles.shoutHeader}>
+            <div class={styles.shoutTopic}>
+              <a href={`/topic/${props.article.mainTopic}`} innerHTML={mainTopicTitle() || ''} />
+            </div>
 
-          <h1>{props.article.title}</h1>
-          <Show when={props.article.subtitle}>
-            <h4>{capitalize(props.article.subtitle, false)}</h4>
-          </Show>
+            <h1>{props.article.title}</h1>
+            <Show when={props.article.subtitle}>
+              <h4>{capitalize(props.article.subtitle, false)}</h4>
+            </Show>
 
-          <div class={styles.shoutAuthor}>
-            <For each={props.article.authors}>
-              {(a: Author, index) => (
-                <>
-                  <Show when={index() > 0}>, </Show>
-                  <a href={`/author/${a.slug}`}>{a.name}</a>
-                </>
-              )}
-            </For>
-          </div>
-          <div class={styles.shoutCover} style={{ 'background-image': `url('${props.article.cover}')` }} />
-        </div>
-
-        <Show
-          when={media() && props.article.layout !== 'image'}
-          fallback={
-            <Slider>
-              <For each={media() || []}>
-                {(m: MediaItem) => (
+            <div class={styles.shoutAuthor}>
+              <For each={props.article.authors}>
+                {(a: Author, index) => (
                   <>
-                    <img src={m.url || m.pic} alt={m.title} />
-                    <div innerHTML={m.body} />
+                    <Show when={index() > 0}>, </Show>
+                    <a href={`/author/${a.slug}`}>{a.name}</a>
                   </>
                 )}
               </For>
-            </Slider>
-          }
-        >
-          <div class="media-items">
-            <For each={media() || []}>
-              {(m: MediaItem) => (
-                <div class={styles.shoutMediaBody}>
-                  <MediaView media={m} kind={props.article.layout} />
-                  <Show when={m?.body}>
-                    <div innerHTML={m.body} />
-                  </Show>
+            </div>
+            <div
+              class={styles.shoutCover}
+              style={{ 'background-image': `url('${props.article.cover}')` }}
+            />
+          </div>
+
+          <Show when={media() && props.article.layout !== 'image'}>
+            <div class="media-items">
+              <For each={media() || []}>
+                {(m: MediaItem) => (
+                  <div class={styles.shoutMediaBody}>
+                    <MediaView media={m} kind={props.article.layout} />
+                    <Show when={m?.body}>
+                      <div innerHTML={m.body} />
+                    </Show>
+                  </div>
+                )}
+              </For>
+            </div>
+          </Show>
+          <Show when={body()}>
+            <div class={styles.shoutBody}>
+              <Show when={!body().startsWith('<')} fallback={<div innerHTML={body()} />}>
+                <MD body={body()} />
+              </Show>
+            </div>
+          </Show>
+        </article>
+      </div>
+
+      <Show when={media() && props.article.layout === 'image'}>
+        <Slider slidesPerView={1} isPageGallery={true} isCardsWithCover={true} hasThumbs={true}>
+          <For each={media() || []}>
+            {(m: MediaItem) => (
+              <div class="swiper-slide">
+                <div class="swiper-slide__inner">
+                  <img src={m.url || m.pic} alt={m.title} loading="lazy" />
+                  <div class="swiper-lazy-preloader swiper-lazy-preloader-white" />
+                  <div class="image-description" innerHTML={m.title} />
+                </div>
+              </div>
+            )}
+          </For>
+        </Slider>
+      </Show>
+
+      <div class="shout wide-container">
+        <div class="col-md-8 shift-content">
+          <div class={styles.shoutStats}>
+            <div class={styles.shoutStatsItem}>
+              <RatingControl rating={props.article.stat?.rating} class={styles.ratingControl} />
+            </div>
+
+            <Show when={props.article.stat?.viewed}>
+              <div class={clsx(styles.shoutStatsItem)}>
+                <Icon name="eye" class={clsx(styles.icon, styles.iconEye)} />
+                {props.article.stat?.viewed}
+              </div>
+            </Show>
+
+            <div class={styles.shoutStatsItem}>
+              <Icon name="comment" class={styles.icon} />
+              {props.article.stat?.commented || ''}
+            </div>
+
+            <div class={styles.shoutStatsItem}>
+              <SharePopup
+                title={props.article.title}
+                description={getDescription(props.article.body)}
+                imageUrl={props.article.cover}
+                containerCssClass={stylesHeader.control}
+                trigger={<Icon name="share-outline" class={styles.icon} />}
+              />
+            </div>
+
+            <div class={styles.shoutStatsItem} onClick={bookmark}>
+              <Icon name="bookmark" class={styles.icon} />
+            </div>
+
+            <Show when={canEdit()}>
+              <div class={styles.shoutStatsItem}>
+                <a href="/edit">
+                  <Icon name="edit" />
+                  {t('Edit')}
+                </a>
+              </div>
+            </Show>
+            <div class={clsx(styles.shoutStatsItem, styles.shoutStatsItemAdditionalData)}>
+              <div class={clsx(styles.shoutStatsItem, styles.shoutStatsItemAdditionalDataItem)}>
+                {formattedDate()}
+              </div>
+            </div>
+          </div>
+          <div class={styles.help}>
+            <Show when={session()?.token}>
+              <button class="button">{t('Cooperate')}</button>
+            </Show>
+            <Show when={canEdit()}>
+              <button class="button button--light">{t('Invite to collab')}</button>
+            </Show>
+          </div>
+
+          <div class={styles.topicsList}>
+            <For each={props.article.topics}>
+              {(topic) => (
+                <div class={styles.shoutTopic}>
+                  <a href={`/topic/${topic.slug}`}>{topic.title}</a>
                 </div>
               )}
             </For>
           </div>
-        </Show>
-        <Show when={body()}>
-          <div class={styles.shoutBody}>
-            <Show when={!body().startsWith('<')} fallback={<div innerHTML={body()} />}>
-              <MD body={body()} />
+
+          <div class={styles.shoutAuthorsList}>
+            <Show when={props.article?.authors?.length > 1}>
+              <h4>{t('Authors')}</h4>
             </Show>
+            <For each={props.article?.authors}>
+              {(a: Author) => (
+                <div class="col-xl-6">
+                  <AuthorCard author={a} compact={false} hasLink={true} liteButtons={true} />
+                </div>
+              )}
+            </For>
           </div>
-        </Show>
-      </article>
-
-      <div class="col-md-8 shift-content">
-        <div class={styles.shoutStats}>
-          <div class={styles.shoutStatsItem}>
-            <RatingControl rating={props.article.stat?.rating} class={styles.ratingControl} />
-          </div>
-
-          <Show when={props.article.stat?.viewed}>
-            <div class={clsx(styles.shoutStatsItem)}>
-              <Icon name="eye" class={clsx(styles.icon, styles.iconEye)} />
-              {props.article.stat?.viewed}
-            </div>
-          </Show>
-
-          <div class={styles.shoutStatsItem}>
-            <Icon name="comment" class={styles.icon} />
-            {props.article.stat?.commented || ''}
-          </div>
-
-          <div class={styles.shoutStatsItem}>
-            <SharePopup
-              title={props.article.title}
-              description={getDescription(props.article.body)}
-              imageUrl={props.article.cover}
-              containerCssClass={stylesHeader.control}
-              trigger={<Icon name="share-outline" class={styles.icon} />}
-            />
-          </div>
-
-          <div class={styles.shoutStatsItem} onClick={bookmark}>
-            <Icon name="bookmark" class={styles.icon} />
-          </div>
-
-          <Show when={canEdit()}>
-            <div class={styles.shoutStatsItem}>
-              <a href="/edit">
-                <Icon name="edit" />
-                {t('Edit')}
-              </a>
-            </div>
-          </Show>
-          <div class={clsx(styles.shoutStatsItem, styles.shoutStatsItemAdditionalData)}>
-            <div class={clsx(styles.shoutStatsItem, styles.shoutStatsItemAdditionalDataItem)}>
-              {formattedDate()}
-            </div>
-          </div>
+          <CommentsTree
+            shoutId={props.article?.id}
+            shoutSlug={props.article?.slug}
+            commentAuthors={props.article?.authors}
+          />
         </div>
-        <div class={styles.help}>
-          <Show when={session()?.token}>
-            <button class="button">{t('Cooperate')}</button>
-          </Show>
-          <Show when={canEdit()}>
-            <button class="button button--light">{t('Invite to collab')}</button>
-          </Show>
-        </div>
-
-        <div class={styles.topicsList}>
-          <For each={props.article.topics}>
-            {(topic) => (
-              <div class={styles.shoutTopic}>
-                <a href={`/topic/${topic.slug}`}>{topic.title}</a>
-              </div>
-            )}
-          </For>
-        </div>
-
-        <div class={styles.shoutAuthorsList}>
-          <Show when={props.article?.authors?.length > 1}>
-            <h4>{t('Authors')}</h4>
-          </Show>
-          <For each={props.article?.authors}>
-            {(a: Author) => (
-              <div class="col-xl-6">
-                <AuthorCard author={a} compact={false} hasLink={true} liteButtons={true} />
-              </div>
-            )}
-          </For>
-        </div>
-        <CommentsTree
-          shoutId={props.article?.id}
-          shoutSlug={props.article?.slug}
-          commentAuthors={props.article?.authors}
-        />
       </div>
-    </div>
+    </>
   )
 }

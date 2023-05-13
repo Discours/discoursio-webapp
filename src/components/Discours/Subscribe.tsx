beb543141d8defa7b1c25d977198100b557a272e
Vyacheslav Ananev <vnva.dev@gmail.com>
2023-05-13 21:30:14 +0700

    feat(Subscribe): add email and serve response validation

--- src/components/Discours/Subscribe.tsx
+++ src/components/Discours/Subscribe.tsx
@@ -3,12 +3,35 @@ import styles from './Subscribe.module.scss'
 
 import { clsx } from 'clsx'
 import { useLocalize } from '../../context/localize'
+import { isValidEmail } from '../../utils/validators'
 
 export default () => {
   const { t } = useLocalize()
-  let emailElement: HTMLInputElement | undefined
+
   const [title, setTitle] = createSignal('')
-  const subscribe = async () => {
+  const [email, setEmail] = createSignal('')
+  const [emailError, setEmailError] = createSignal<string>(null)
+
+  const validate = (): boolean => {
+    if (!email()) {
+      setEmailError(t('Please enter email'))
+      return false
+    }
+
+    if (!isValidEmail(email())) {
+      setEmailError(t('Please check your email address'))
+      return false
+    }
+
+    setEmailError(null)
+    return true
+  }
+
+  const subscribe = async (event: SubmitEvent) => {
+    event.preventDefault()
+
+    if (!validate()) return
+
     setTitle(t('...subscribing'))
 
     const requestOptions = {
@@ -16,26 +39,44 @@ export default () => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: emailElement?.value
-      })
+      body: JSON.stringify({ email })
     }
 
-    const r = await fetch('/api/newsletter', requestOptions)
-    setTitle(r.ok ? t('You are subscribed') : '')
+    const response = await fetch('/api/newsletter', requestOptions)
+
+    if (response.ok) {
+      setTitle(t('You are subscribed'))
+    } else {
+      if (response.status === 400) {
+        setEmailError(t('Please check your email address'))
+      } else {
+        setEmailError(t('Something went wrong, please try again'))
+      }
+
+      setTitle('')
+    }
   }
 
   return (
-    <div class={styles.subscribeForm}>
+    <form class={styles.form} onSubmit={subscribe} novalidate>
       <Show when={!title()} fallback={title()}>
-        <input type="email" name="email" ref={emailElement} placeholder={t('Fill email')} />
-        <button
-          class={clsx(styles.button, 'button--light')}
-          onClick={() => emailElement?.value && subscribe()}
-        >
-          {t('Subscribe')}
-        </button>
+        <div class={styles.controls}>
+          <input
+            type="email"
+            name="email"
+            value={email()}
+            onChange={(e) => setEmail(e.target.value)}
+            class={styles.input}
+            placeholder={t('Fill email')}
+          />
+          <button type="submit" class={clsx(styles.button, 'button--light')}>
+            {t('Subscribe')}
+          </button>
+        </div>
+        <Show when={emailError()}>
+          <div class={styles.error}>{emailError()}</div>
+        </Show>
       </Show>
-    </div>
+    </form>
   )
 }

659abb5e21dc0e3ed5f70921e66e023e573189e5
Ilya Y <75578537+ilya-bkv@users.noreply.github.com>
2023-05-31 15:23:18 +0300

    Create page (#106)

--- src/pages/create.page.tsx
+++ src/pages/create.page.tsx
@@ -1,19 +1,58 @@
 import { PageLayout } from '../components/_shared/PageLayout'
-import { Loading } from '../components/_shared/Loading'
-import { onMount } from 'solid-js'
+import { useLocalize } from '../context/localize'
+import { Button } from '../components/_shared/Button'
+import { Icon } from '../components/_shared/Icon'
+import { clsx } from 'clsx'
+import styles from '../styles/Create.module.scss'
 import { apiClient } from '../utils/apiClient'
-import { router } from '../stores/router'
 import { redirectPage } from '@nanostores/router'
+import { router } from '../stores/router'
+
+const handleCreateArticle = async () => {
+  const shout = await apiClient.createArticle({ article: {} })
+  redirectPage(router, 'edit', { shoutId: shout.id.toString() })
+}
 
 export const CreatePage = () => {
-  onMount(async () => {
-    const shout = await apiClient.createArticle({ article: {} })
-    redirectPage(router, 'edit', { shoutId: shout.id.toString() })
-  })
-
+  const { t } = useLocalize()
   return (
     <PageLayout>
-      <Loading />
+      <article class={clsx('wide-container', 'container--static-page', styles.Create)}>
+        <h1>{t('Choose a post type')}</h1>
+        <ul class={clsx('nodash', styles.list)}>
+          <li>
+            <div class={styles.link} onClick={handleCreateArticle}>
+              <Icon name="create-article" class={styles.icon} />
+              <div>{t('article')}</div>
+            </div>
+          </li>
+          <li>
+            <a href="#">
+              <Icon name="create-books" class={styles.icon} />
+              <div>{t('literature')}</div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Icon name="create-images" class={styles.icon} />
+              <div>{t('images')}</div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Icon name="create-music" class={styles.icon} />
+              <div>{t('music')}</div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Icon name="create-video" class={styles.icon} />
+              <div>{t('music')}</div>
+            </a>
+          </li>
+        </ul>
+        <Button value={t('Back')} onClick={() => window.history.back()} />
+      </article>
     </PageLayout>
   )
 }

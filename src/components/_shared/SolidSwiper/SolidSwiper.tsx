b9f2ebb3d766517ddbbd6484dc7d36829b420ba9
ilya-bkv <i.yablokov@ccmp.me>
2023-06-30 16:04:40 +0300

    conditional render

--- src/components/_shared/SolidSwiper/SolidSwiper.tsx
+++ src/components/_shared/SolidSwiper/SolidSwiper.tsx
@@ -163,191 +163,196 @@ export const SolidSwiper = (props: Props) => {
             }
           />
         </Show>
-
-        <div class={styles.holder}>
-          <swiper-container
-            ref={(el) => (mainSwipeRef.current = el)}
-            slides-per-view={1}
-            thumbs-swiper={'.thumbSwiper'}
-            observer={true}
-            onSlideChange={handleSlideChange}
-          >
-            <For each={props.images}>
-              {(slide, index) => (
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                <swiper-slide lazy="true" virtual-index={index()}>
-                  <div class={styles.image}>
-                    <img src={slide.url} alt={slide.title} />
-                    <Show when={props.editorMode}>
-                      <Popover content={t('Delete')}>
-                        {(triggerRef: (el) => void) => (
-                          <div ref={triggerRef} onClick={() => handleDelete(index())} class={styles.action}>
-                            <Icon class={styles.icon} name="delete-white" />
-                          </div>
-                        )}
-                      </Popover>
-                    </Show>
-                  </div>
-                  <Switch>
-                    <Match when={props.editorMode}>
-                      <div class={styles.description}>
-                        <input
-                          type="text"
-                          class={clsx(styles.input, styles.title)}
-                          placeholder={t('Enter image title')}
-                          value={slide.title}
-                          onChange={(event) =>
-                            handleSlideDescriptionChange(index(), 'title', event.target.value)
-                          }
-                        />
-                        <input
-                          type="text"
-                          class={styles.input}
-                          placeholder={t('Specify the source and the name of the author')}
-                          value={slide.source}
-                          onChange={(event) =>
-                            handleSlideDescriptionChange(index(), 'source', event.target.value)
-                          }
-                        />
-                        <GrowingTextarea
-                          class={styles.descriptionText}
-                          placeholder={t('Enter image description')}
-                          initialValue={slide.body}
-                          value={(value) => handleSlideDescriptionChange(index(), 'body', value)}
-                        />
-                      </div>
-                    </Match>
-                    <Match when={!props.editorMode}>
-                      <div class={styles.slideDescription}>
-                        <Show when={slide?.title}>
-                          <div class={styles.articleTitle}>
-                            <small>{slide.title}</small>
-                          </div>
-                        </Show>
-                        <Show when={slide?.source}>
-                          <div class={styles.source}>
-                            <small>{slide.source}</small>
-                          </div>
-                        </Show>
-                        <Show when={slide?.body}>
-                          <div class={styles.body}>
-                            <MD body={slide.body} />
-                          </div>
-                        </Show>
-                      </div>
-                    </Match>
-                  </Switch>
-                </swiper-slide>
-              )}
-            </For>
-          </swiper-container>
-          <div
-            class={clsx(styles.navigation, styles.prev, {
-              [styles.disabled]: slideIndex() === 0
-            })}
-            onClick={() => mainSwipeRef.current.swiper.slidePrev()}
-          >
-            <Icon name="swiper-l-arr" class={styles.icon} />
-          </div>
-          <div
-            class={clsx(styles.navigation, styles.next, {
-              [styles.disabled]: slideIndex() + 1 === props.images.length
-            })}
-            onClick={() => mainSwipeRef.current.swiper.slideNext()}
-          >
-            <Icon name="swiper-r-arr" class={styles.icon} />
-          </div>
-          <div class={styles.counter}>
-            {slideIndex() + 1} / {props.images.length}
-          </div>
-        </div>
-
-        <div class={clsx(styles.holder, styles.thumbsHolder)}>
-          <div class={styles.thumbs}>
+        <Show when={props.images.length > 0}>
+          <div class={styles.holder}>
             <swiper-container
-              class={'thumbSwiper'}
-              ref={(el) => (thumbSwipeRef.current = el)}
-              slides-per-view={'auto'}
-              free-mode={true}
+              ref={(el) => (mainSwipeRef.current = el)}
+              slides-per-view={1}
+              thumbs-swiper={'.thumbSwiper'}
               observer={true}
-              space-between={20}
-              auto-scroll-offset={1}
-              watch-overflow={true}
-              slide-to-clicked-slide={true}
-              watch-slides-visibility={true}
-              watch-slides-progress={true}
-              direction={props.editorMode ? 'horizontal' : 'vertical'}
-              slides-offset-after={props.editorMode && 140}
+              onSlideChange={handleSlideChange}
             >
               <For each={props.images}>
                 {(slide, index) => (
                   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                   // @ts-ignore
-                  <swiper-slide virtual-index={index()} style={{ width: 'auto', height: 'auto' }}>
-                    <div
-                      class={clsx(styles.imageThumb)}
-                      style={{ 'background-image': `url(${slide.url})` }}
-                    >
+                  <swiper-slide lazy="true" virtual-index={index()}>
+                    <div class={styles.image}>
+                      <img src={slide.url} alt={slide.title} />
                       <Show when={props.editorMode}>
-                        <div class={styles.thumbAction}>
-                          <div class={clsx(styles.action)} onClick={() => handleDelete(index())}>
-                            <Icon class={styles.icon} name="delete-white" />
-                          </div>
-                          <div
-                            class={clsx(styles.action, {
-                              [styles.hidden]: index() === 0
-                            })}
-                            onClick={() => handleChangeIndex('left', index())}
-                          >
-                            <Icon
-                              class={styles.icon}
-                              name="arrow-right-white"
-                              style={{ transform: 'rotate(-180deg)' }}
-                            />
-                          </div>
-                          <div
-                            class={clsx(styles.action, {
-                              [styles.hidden]: index() + 1 === Number(props.images.length)
-                            })}
-                            onClick={() => handleChangeIndex('right', index())}
-                          >
-                            <Icon class={styles.icon} name="arrow-right-white" />
-                          </div>
-                        </div>
+                        <Popover content={t('Delete')}>
+                          {(triggerRef: (el) => void) => (
+                            <div
+                              ref={triggerRef}
+                              onClick={() => handleDelete(index())}
+                              class={styles.action}
+                            >
+                              <Icon class={styles.icon} name="delete-white" />
+                            </div>
+                          )}
+                        </Popover>
                       </Show>
                     </div>
+                    <Switch>
+                      <Match when={props.editorMode}>
+                        <div class={styles.description}>
+                          <input
+                            type="text"
+                            class={clsx(styles.input, styles.title)}
+                            placeholder={t('Enter image title')}
+                            value={slide.title}
+                            onChange={(event) =>
+                              handleSlideDescriptionChange(index(), 'title', event.target.value)
+                            }
+                          />
+                          <input
+                            type="text"
+                            class={styles.input}
+                            placeholder={t('Specify the source and the name of the author')}
+                            value={slide.source}
+                            onChange={(event) =>
+                              handleSlideDescriptionChange(index(), 'source', event.target.value)
+                            }
+                          />
+                          <GrowingTextarea
+                            class={styles.descriptionText}
+                            placeholder={t('Enter image description')}
+                            initialValue={slide.body}
+                            value={(value) => handleSlideDescriptionChange(index(), 'body', value)}
+                          />
+                        </div>
+                      </Match>
+                      <Match when={!props.editorMode}>
+                        <div class={styles.slideDescription}>
+                          <Show when={slide?.title}>
+                            <div class={styles.articleTitle}>
+                              <small>{slide.title}</small>
+                            </div>
+                          </Show>
+                          <Show when={slide?.source}>
+                            <div class={styles.source}>
+                              <small>{slide.source}</small>
+                            </div>
+                          </Show>
+                          <Show when={slide?.body}>
+                            <div class={styles.body}>
+                              <MD body={slide.body} />
+                            </div>
+                          </Show>
+                        </div>
+                      </Match>
+                    </Switch>
                   </swiper-slide>
                 )}
               </For>
-              <Show when={props.editorMode}>
-                <div class={styles.upload}>
-                  <div class={styles.inner} onClick={handleUploadThumb}>
-                    <Show when={!loading()} fallback={<Loading size="small" />}>
-                      <Icon name="swiper-plus" />
-                    </Show>
-                  </div>
-                </div>
-              </Show>
             </swiper-container>
             <div
-              class={clsx(styles.navigation, styles.thumbsNav, styles.prev, {
+              class={clsx(styles.navigation, styles.prev, {
                 [styles.disabled]: slideIndex() === 0
               })}
-              onClick={() => thumbSwipeRef.current.swiper.slidePrev()}
+              onClick={() => mainSwipeRef.current.swiper.slidePrev()}
             >
-              <Icon iconClassName={styles.icon} name="swiper-l-arr" class={styles.icon} />
+              <Icon name="swiper-l-arr" class={styles.icon} />
             </div>
             <div
-              class={clsx(styles.navigation, styles.thumbsNav, styles.next, {
-                [styles.disabled]: slideIndex() + 1 === Number(props.images.length)
+              class={clsx(styles.navigation, styles.next, {
+                [styles.disabled]: slideIndex() + 1 === props.images.length
               })}
-              onClick={() => thumbSwipeRef.current.swiper.slideNext()}
+              onClick={() => mainSwipeRef.current.swiper.slideNext()}
             >
-              <Icon name="swiper-r-arr" iconClassName={styles.icon} class={styles.icon} />
+              <Icon name="swiper-r-arr" class={styles.icon} />
+            </div>
+            <div class={styles.counter}>
+              {slideIndex() + 1} / {props.images.length}
             </div>
           </div>
-        </div>
+
+          <div class={clsx(styles.holder, styles.thumbsHolder)}>
+            <div class={styles.thumbs}>
+              <swiper-container
+                class={'thumbSwiper'}
+                ref={(el) => (thumbSwipeRef.current = el)}
+                slides-per-view={'auto'}
+                free-mode={true}
+                observer={true}
+                space-between={20}
+                auto-scroll-offset={1}
+                watch-overflow={true}
+                slide-to-clicked-slide={true}
+                watch-slides-visibility={true}
+                watch-slides-progress={true}
+                direction={props.editorMode ? 'horizontal' : 'vertical'}
+                slides-offset-after={props.editorMode && 140}
+              >
+                <For each={props.images}>
+                  {(slide, index) => (
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-ignore
+                    <swiper-slide virtual-index={index()} style={{ width: 'auto', height: 'auto' }}>
+                      <div
+                        class={clsx(styles.imageThumb)}
+                        style={{ 'background-image': `url(${slide.url})` }}
+                      >
+                        <Show when={props.editorMode}>
+                          <div class={styles.thumbAction}>
+                            <div class={clsx(styles.action)} onClick={() => handleDelete(index())}>
+                              <Icon class={styles.icon} name="delete-white" />
+                            </div>
+                            <div
+                              class={clsx(styles.action, {
+                                [styles.hidden]: index() === 0
+                              })}
+                              onClick={() => handleChangeIndex('left', index())}
+                            >
+                              <Icon
+                                class={styles.icon}
+                                name="arrow-right-white"
+                                style={{ transform: 'rotate(-180deg)' }}
+                              />
+                            </div>
+                            <div
+                              class={clsx(styles.action, {
+                                [styles.hidden]: index() + 1 === Number(props.images.length)
+                              })}
+                              onClick={() => handleChangeIndex('right', index())}
+                            >
+                              <Icon class={styles.icon} name="arrow-right-white" />
+                            </div>
+                          </div>
+                        </Show>
+                      </div>
+                    </swiper-slide>
+                  )}
+                </For>
+                <Show when={props.editorMode}>
+                  <div class={styles.upload}>
+                    <div class={styles.inner} onClick={handleUploadThumb}>
+                      <Show when={!loading()} fallback={<Loading size="small" />}>
+                        <Icon name="swiper-plus" />
+                      </Show>
+                    </div>
+                  </div>
+                </Show>
+              </swiper-container>
+              <div
+                class={clsx(styles.navigation, styles.thumbsNav, styles.prev, {
+                  [styles.disabled]: slideIndex() === 0
+                })}
+                onClick={() => thumbSwipeRef.current.swiper.slidePrev()}
+              >
+                <Icon iconClassName={styles.icon} name="swiper-l-arr" class={styles.icon} />
+              </div>
+              <div
+                class={clsx(styles.navigation, styles.thumbsNav, styles.next, {
+                  [styles.disabled]: slideIndex() + 1 === Number(props.images.length)
+                })}
+                onClick={() => thumbSwipeRef.current.swiper.slideNext()}
+              >
+                <Icon name="swiper-r-arr" iconClassName={styles.icon} class={styles.icon} />
+              </div>
+            </div>
+          </div>
+        </Show>
       </div>
     </div>
   )

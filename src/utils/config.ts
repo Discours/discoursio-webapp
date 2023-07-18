9df0574c4e7f19bb9027f1510dc36ee67df5a95b
Igor Lobanov <bniwredyc@gmail.com>
2023-07-18 15:33:23 +0200

    added sentry (#133)
    * added sentry
    
    * sentry downgrade

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,5 @@ export const isDev = import.meta.env.MODE === 'development'
 
 const defaultApiUrl = 'https://testapi.discours.io'
 export const apiBaseUrl = import.meta.env.PUBLIC_API_URL || defaultApiUrl
+
+export const SENTRY_DSN = import.meta.env.PUBLIC_SENTRY_DSN || ''

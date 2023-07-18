44e8930a40b3bcbdb75f55ddb21213e6837ce748
ilya-bkv <i.yablokov@ccmp.me>
2023-07-18 13:34:59 +0300

    Update composeMediaItems title with remove File Extension

--- src/utils/composeMediaItems.ts
+++ src/utils/composeMediaItems.ts
@@ -1,11 +1,15 @@
 import { UploadedFile } from '../pages/types'
 
+const removeFileExtension = (fileName: string) => {
+  return fileName.replace(/\.(wav|flac|mp3|aac|jpg|jpeg|png|gif)$/i, '')
+}
+
 export const composeMediaItems = (value: UploadedFile[], optionalParams = {}) => {
   return value.map((fileData) => {
     return {
       url: fileData.url,
       source: '',
-      title: fileData.originalFilename,
+      title: removeFileExtension(fileData.originalFilename),
       body: '',
       ...optionalParams
     }

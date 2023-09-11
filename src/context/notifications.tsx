a6c3fe7f5226ba508fd386512dbbc31732003b5b
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-09-11 11:10:18 +0200

    WIP

--- src/context/notifications.tsx
+++ src/context/notifications.tsx
@@ -1,8 +1,14 @@
 import type { Accessor, JSX } from 'solid-js'
-import { createContext, createSignal, useContext } from 'solid-js'
+import { createContext, createEffect, createSignal, useContext } from 'solid-js'
+import { useSession } from './session'
+import SSEService, { EventData } from '../utils/sseService'
+
+type Notification = {
+  id: number
+}
 
 type NotificationsContextType = {
-  actions: {}
+  notificationEntities: Record<number, Notification>
 }
 
 const NotificationsContext = createContext<NotificationsContextType>()
@@ -11,10 +17,27 @@ export function useNotifications() {
   return useContext(NotificationsContext)
 }
 
-export const NotificationsProvider = (props: { children: JSX.Element }) => {
-  const actions = {}
+const sseService = new SSEService()
 
-  const value: NotificationsContextType = { actions }
+export const NotificationsProvider = (props: { children: JSX.Element }) => {
+  const { isAuthenticated } = useSession()
+
+  createEffect(() => {
+    if (isAuthenticated()) {
+      // Connect to the SSE endpoint and start listening for events
+      sseService.connect(`${process.env.PUBLIC_API_URL}/subscribe`)
+      sseService.subscribeToEvent('message', (data: EventData) => {
+        console.log(data)
+      })
+    } else {
+      // Disconnect from the SSE endpoint
+      sseService.disconnect()
+    }
+  })
+
+  const notificationEntities = {}
+
+  const value: NotificationsContextType = { notificationEntities }
 
   return <NotificationsContext.Provider value={value}>{props.children}</NotificationsContext.Provider>
 }

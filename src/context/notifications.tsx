45bedcc8835aa974217990be815c88967ab14f3e
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-09-11 23:06:35 +0200

    WIP

--- src/context/notifications.tsx
+++ src/context/notifications.tsx
@@ -2,6 +2,7 @@ import type { Accessor, JSX } from 'solid-js'
 import { createContext, createEffect, createSignal, useContext } from 'solid-js'
 import { useSession } from './session'
 import SSEService, { EventData } from '../utils/sseService'
+import { apiBaseUrl } from '../utils/config'
 
 type Notification = {
   id: number
@@ -20,17 +21,16 @@ export function useNotifications() {
 const sseService = new SSEService()
 
 export const NotificationsProvider = (props: { children: JSX.Element }) => {
-  const { isAuthenticated } = useSession()
+  const { isAuthenticated, user } = useSession()
 
   createEffect(() => {
     if (isAuthenticated()) {
-      // Connect to the SSE endpoint and start listening for events
-      sseService.connect(`${process.env.PUBLIC_API_URL}/subscribe`)
+      console.log(user())
+      sseService.connect(`${apiBaseUrl}/subscribe/${user().id}`)
       sseService.subscribeToEvent('message', (data: EventData) => {
         console.log(data)
       })
     } else {
-      // Disconnect from the SSE endpoint
       sseService.disconnect()
     }
   })

218c190d2ed6d9322be07990ea6f4a755c514f6a
Ilya Y <75578537+ilya-bkv@users.noreply.github.com>
2023-09-15 12:30:50 +0300

    Profile settings fixes (#219)

--- src/components/_shared/GrowingTextarea/GrowingTextarea.tsx
+++ src/components/_shared/GrowingTextarea/GrowingTextarea.tsx
@@ -40,45 +40,43 @@ export const GrowingTextarea = (props: Props) => {
   }
 
   return (
-    <Show when={value()}>
-      <div
-        class={clsx(styles.GrowingTextarea, {
-          [styles.bordered]: props.variant === 'bordered',
-          [styles.hasFieldName]: props.fieldName && value().length > 0
-        })}
-      >
-        <Show when={props.fieldName && value().length > 0}>
-          <div class={styles.fieldName}>{props.fieldName}</div>
-        </Show>
-        <div class={clsx(styles.growWrap, props.class)} data-replicated-value={value()}>
-          <textarea
-            ref={props.textAreaRef}
-            rows={1}
-            maxlength={props.maxLength}
-            autocomplete="off"
-            class={clsx(styles.textInput, props.class)}
-            value={props.maxLength ? props.initialValue.slice(0, props.maxLength) : props.initialValue}
-            onKeyDown={props.allowEnterKey ? handleKeyDown : null}
-            onInput={(event) => handleChangeValue(event)}
-            onChange={(event) => props.value(event.target.value)}
-            placeholder={props.placeholder}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-          />
-        </div>
-        <Show when={(props.maxLength && value() && isFocused()) || props.variant === 'bordered'}>
-          <div
-            class={clsx(styles.maxLength, {
-              [styles.visible]: isFocused(),
-              [styles.limited]: value().length === props.maxLength
-            })}
-          >
-            <Show when={props.variant === 'bordered'} fallback={`${value().length} / ${props.maxLength}`}>
-              {`${props.maxLength - value().length}`}
-            </Show>
-          </div>
-        </Show>
+    <div
+      class={clsx(styles.GrowingTextarea, {
+        [styles.bordered]: props.variant === 'bordered',
+        [styles.hasFieldName]: props.fieldName && value().length > 0
+      })}
+    >
+      <Show when={props.fieldName && value().length > 0}>
+        <div class={styles.fieldName}>{props.fieldName}</div>
+      </Show>
+      <div class={clsx(styles.growWrap, props.class)} data-replicated-value={value()}>
+        <textarea
+          ref={props.textAreaRef}
+          rows={1}
+          maxlength={props.maxLength}
+          autocomplete="off"
+          class={clsx(styles.textInput, props.class)}
+          value={props.maxLength ? props.initialValue.slice(0, props.maxLength) : props.initialValue}
+          onKeyDown={props.allowEnterKey ? handleKeyDown : null}
+          onInput={(event) => handleChangeValue(event)}
+          onChange={(event) => props.value(event.target.value)}
+          placeholder={props.placeholder}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
       </div>
-    </Show>
+      <Show when={(props.maxLength && value() && isFocused()) || props.variant === 'bordered'}>
+        <div
+          class={clsx(styles.maxLength, {
+            [styles.visible]: isFocused(),
+            [styles.limited]: value().length === props.maxLength
+          })}
+        >
+          <Show when={props.variant === 'bordered'} fallback={`${value().length} / ${props.maxLength}`}>
+            {`${props.maxLength - value().length}`}
+          </Show>
+        </div>
+      </Show>
+    </div>
   )
 }

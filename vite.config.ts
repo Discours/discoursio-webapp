0989dc9762b9070634fb931b28815c3bfcae2893
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-09-27 01:04:59 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,8 @@ export default defineConfig(() => {
         'i18next',
         'js-cookie',
         '@solid-primitives/memo',
-        '@solid-primitives/media'
+        '@solid-primitives/media',
+        '@solid-primitives/storage'
       ]
     }
   }

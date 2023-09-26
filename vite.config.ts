9ede90d5e498f54491ca9ba1390e663cc69ac6f7
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-09-27 00:43:48 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(() => {
       target: 'esnext'
     },
     ssr: {
-      noExternal: ['solid-js', '@nanostores/solid', '@urql/core', 'wonka', 'solid-popper']
+      noExternal: ['solid-js', '@nanostores/solid', '@urql/core', 'wonka', 'solid-popper', 'seroval']
     }
   }
 })

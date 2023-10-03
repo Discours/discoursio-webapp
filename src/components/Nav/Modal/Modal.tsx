b0b0136f3417ac29f3f8fe802d28dc2818ed1c35
kvakazyambra <kvakazyambra@gmail.com>
2023-10-04 00:52:38 +0300

    Modal style fixes

--- src/components/Nav/Modal/Modal.tsx
+++ src/components/Nav/Modal/Modal.tsx
@@ -10,7 +10,7 @@ import { router } from '../../../stores/router'
 
 interface Props {
   name: string
-  variant: 'narrow' | 'wide'
+  variant: 'narrow' | 'wide' | 'medium'
   children: JSX.Element
   onClose?: () => void
   noPadding?: boolean
@@ -43,28 +43,31 @@ export const Modal = (props: Props) => {
   return (
     <Show when={visible()}>
       <div class={styles.backdrop} onClick={handleHide}>
-        <div
-          class={clsx(styles.modal, {
-            [styles.narrow]: props.variant === 'narrow',
-            [styles.noPadding]: props.noPadding,
-            [styles.maxHeight]: props.maxHeight
-          })}
-          onClick={(event) => event.stopPropagation()}
-        >
-          {props.children}
-          <div class={styles.close} onClick={handleHide}>
-            <svg
-              class={styles.icon}
-              width="16"
-              height="18"
-              viewBox="0 0 16 18"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.99987 7.52552L14.1871 0.92334L15.9548 2.80968L9.76764 9.41185L15.9548 16.014L14.1871 17.9004L7.99987 11.2982L1.81269 17.9004L0.0449219 16.014L6.23211 9.41185L0.0449225 2.80968L1.81269 0.92334L7.99987 7.52552Z"
-                fill="currentColor"
-              />
-            </svg>
+        <div className="wide-container">
+          <div
+            class={clsx(styles.modal, {
+              [styles.narrow]: props.variant === 'narrow',
+              ['col-auto col-md-20 offset-md-2 col-lg-14 offset-lg-5']: props.variant === 'medium',
+              [styles.noPadding]: props.noPadding,
+              [styles.maxHeight]: props.maxHeight
+            })}
+            onClick={(event) => event.stopPropagation()}
+          >
+            <div class={styles.modalInner}>{props.children}</div>
+            <div class={styles.close} onClick={handleHide}>
+              <svg
+                class={styles.icon}
+                width="16"
+                height="18"
+                viewBox="0 0 16 18"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.99987 7.52552L14.1871 0.92334L15.9548 2.80968L9.76764 9.41185L15.9548 16.014L14.1871 17.9004L7.99987 11.2982L1.81269 17.9004L0.0449219 16.014L6.23211 9.41185L0.0449225 2.80968L1.81269 0.92334L7.99987 7.52552Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>

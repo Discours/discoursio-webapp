d9970de2f829714b112ad142e9a3880cb27c4b88
ilya-bkv <i.yablokov@ccmp.me>
2023-10-16 18:54:40 +0300

    Cleanup code

--- src/components/Nav/ConfirmModal/ConfirmModal.tsx
+++ src/components/Nav/ConfirmModal/ConfirmModal.tsx
@@ -1,8 +1,7 @@
-import { clsx } from 'clsx'
 import { useConfirm } from '../../../context/confirm'
-import styles from './ConfirmModal.module.scss'
 import { useLocalize } from '../../../context/localize'
 import { Button } from '../../_shared/Button'
+import styles from './ConfirmModal.module.scss'
 
 export const ConfirmModal = () => {
   const { t } = useLocalize()

a381ef12ed3d7a3fcd58c57e3f22c542e93bf3dd
ilya-bkv <i.yablokov@ccmp.me>
2023-10-17 16:18:20 +0300

    Change rect top

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -16,12 +16,7 @@ interface Props {
 
 const isInViewport = (el: Element): boolean => {
   const rect = el.getBoundingClientRect()
-  return (
-    rect.top <= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  )
+  return rect.top <= 0
 }
 const scrollToHeader = (element) => {
   window.scrollTo({

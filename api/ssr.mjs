b751109bd75e3d989fdd53066b9ed6f5fcb1a16c
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-11-09 14:06:04 +0100

    COOP header

--- api/ssr.mjs
+++ api/ssr.mjs
@@ -23,5 +23,6 @@ export default async function handler(req, res) {
   const { body, statusCode, contentType } = httpResponse
   res.statusCode = statusCode
   res.setHeader('Content-Type', contentType)
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
   res.end(body)
 }

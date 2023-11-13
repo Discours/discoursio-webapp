f2d7f1d895f1f9aae13c8fb74bb3f3e811394415
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-11-13 18:58:30 +0100

    test

--- api/ssr.mjs
+++ api/ssr.mjs
@@ -26,7 +26,6 @@ export default async function handler(req, res) {
   const { body, statusCode, contentType } = httpResponse
   res.statusCode = statusCode
   res.setHeader('Content-Type', contentType)
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
-  res.setHeader('Test', 'test=1')
+  // res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
   res.end(body)
 }

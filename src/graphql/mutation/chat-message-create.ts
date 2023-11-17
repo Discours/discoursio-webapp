99f39abf12aecd0254f0cf2d4703d6b347cc212c
Untone <anton.rewin@gmail.com>
2023-11-17 08:01:23 +0300

    fix-mutation

--- src/graphql/mutation/chat-message-create.ts
+++ src/graphql/mutation/chat-message-create.ts
@@ -1,8 +1,8 @@
 import { gql } from '@urql/core'
 
 export default gql`
-  mutation createMessage($chat: String!, $body: String!, $reply_to: Int) {
-    createMessage(chat: $chat, body: $body, reply_to: $reply_to) {
+  mutation createMessage($chat_id: String!, $body: String!, $reply_to: Int) {
+    createMessage(chat_id: $chat_id, body: $body, reply_to: $reply_to) {
       error
       message {
         id

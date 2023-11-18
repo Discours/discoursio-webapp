5beb1eb31aa1619a8704dce60e4b8096c2388c36
Igor Lobanov <igor.lobanov@onetwotrip.com>
2023-11-18 16:17:29 +0100

    404 page made right

--- src/pages/author.page.server.ts
+++ src/pages/author.page.server.ts
@@ -1,17 +1,24 @@
 import type { PageProps } from './types'
 import type { PageContext } from '../renderer/types'
 
+import { render } from 'vike/abort'
+
 import { PRERENDERED_ARTICLES_COUNT } from '../components/Views/Author'
 import { apiClient } from '../utils/apiClient'
 
 export const onBeforeRender = async (pageContext: PageContext) => {
   const { slug } = pageContext.routeParams
 
+  const author = await apiClient.getAuthor({ slug })
+
+  if (!author) {
+    throw render(404)
+  }
+
   const authorShouts = await apiClient.getShouts({
     filters: { author: slug, visibility: 'community' },
     limit: PRERENDERED_ARTICLES_COUNT,
   })
-  const author = await apiClient.getAuthor({ slug })
 
   const pageProps: PageProps = { author, authorShouts, seo: { title: author.name } }
 

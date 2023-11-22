57a289e38389d0d9a5d8c32bfde0c84a415b48be
kvakazyambra <kvakazyambra@gmail.com>
2023-11-23 00:31:51 +0300

    Lightbox controls style fixes

--- src/components/_shared/Lightbox/Lightbox.tsx
+++ src/components/_shared/Lightbox/Lightbox.tsx
@@ -28,6 +28,10 @@ export const Lightbox = (props: Props) => {
     event.stopPropagation()
     setZoomLevel(zoomLevel() / ZOOM_STEP)
   }
+  const zoomReset = (event) => {
+    event.stopPropagation()
+    setZoomLevel(1)
+  }
 
   const lightboxStyle = () => ({
     transform: `scale(${zoomLevel()})`,
@@ -39,17 +43,17 @@ export const Lightbox = (props: Props) => {
   return (
     <div class={clsx(styles.Lightbox, props.class)} onClick={closeLightbox}>
       <span class={styles.close} onClick={closeLightbox}>
-        <Icon name="close-white" />
+        <Icon name="close-white" class={styles.icon} />
       </span>
       <div class={styles.zoomControls}>
         <button class={styles.control} onClick={(event) => zoomOut(event)}>
-          <b>-</b>
+          &minus;
         </button>
-        <button class={styles.control} onClick={() => setZoomLevel(1)}>
-          <b style={{ 'font-size': '10px' }}>1:1</b>
+        <button class={clsx(styles.control, styles.controlDefault)} onClick={(event) => zoomReset(event)}>
+          1:1
         </button>
         <button class={styles.control} onClick={(event) => zoomIn(event)}>
-          <b>+</b>
+          +
         </button>
       </div>
       <img

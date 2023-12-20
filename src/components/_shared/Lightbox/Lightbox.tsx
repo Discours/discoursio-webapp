afdce62585289a5158706fb023482c19d988bc43
dog <arkadyrakovsky@gmail.com>
2023-12-20 17:12:04 +0300

    upgrade lightbox

--- src/components/_shared/Lightbox/Lightbox.tsx
+++ src/components/_shared/Lightbox/Lightbox.tsx
@@ -1,36 +1,63 @@
 import { clsx } from 'clsx'
 import { createSignal } from 'solid-js'
 
-import { useEscKeyDownHandler } from '../../../utils/useEscKeyDownHandler'
 import { Icon } from '../Icon'
+
+import { useEscKeyDownHandler } from '../../../utils/useEscKeyDownHandler'
 
 import styles from './Lightbox.module.scss'
 
 type Props = {
   class?: string
+  imageAlt?: string
   image: string
   onClose: () => void
 }
 
 const ZOOM_STEP = 1.08
+
 export const Lightbox = (props: Props) => {
+  const lightboxRef: {
+    current: HTMLElement
+  } = {
+    current: null,
+  }
+
   const [zoomLevel, setZoomLevel] = createSignal(1)
 
   const closeLightbox = () => {
-    props.onClose()
+    lightboxRef.current?.classList.add(styles.fadeOut)
+
+    setTimeout(() => {
+      props.onClose()
+    }, 300)
   }
 
   const zoomIn = (event) => {
     event.stopPropagation()
     setZoomLevel(zoomLevel() * ZOOM_STEP)
   }
+
   const zoomOut = (event) => {
     event.stopPropagation()
     setZoomLevel(zoomLevel() / ZOOM_STEP)
   }
+
   const zoomReset = (event) => {
     event.stopPropagation()
     setZoomLevel(1)
+  }
+
+  const handleWheelZoom = (event) => {
+    event.preventDefault()
+
+    let scale = zoomLevel()
+
+    scale += event.deltaY * -0.01
+
+    scale = Math.min(Math.max(0.125, scale), 4)
+
+    setZoomLevel(scale * ZOOM_STEP)
   }
 
   const lightboxStyle = () => ({
@@ -41,7 +68,11 @@ export const Lightbox = (props: Props) => {
   useEscKeyDownHandler(closeLightbox)
 
   return (
-    <div class={clsx(styles.Lightbox, props.class)} onClick={closeLightbox}>
+    <div
+      class={clsx(styles.Lightbox, props.class)}
+      onClick={closeLightbox}
+      ref={(el) => (lightboxRef.current = el)}
+    >
       <span class={styles.close} onClick={closeLightbox}>
         <Icon name="close-white" class={styles.icon} />
       </span>
@@ -60,8 +91,9 @@ export const Lightbox = (props: Props) => {
         class={styles.image}
         src={props.image}
         style={lightboxStyle()}
-        alt={''}
+        alt={props.imageAlt || ''}
         onClick={(event) => event.stopPropagation()}
+        onWheel={handleWheelZoom}
       />
     </div>
   )

2bcf41b892bd129029308b9d4b765cc5a262192b
ilya-bkv <i.yablokov@ccmp.me>
2023-12-21 10:31:49 +0300

    Success message

--- src/components/Nav/AuthModal/ChangePasswordForm.tsx
+++ src/components/Nav/AuthModal/ChangePasswordForm.tsx
@@ -1,10 +1,11 @@
 import type { AuthModalSearchParams } from './types'
 
 import { clsx } from 'clsx'
-import { createSignal, JSX } from 'solid-js'
+import { createSignal, JSX, Show } from 'solid-js'
 
 import { useLocalize } from '../../../context/localize'
 import { useRouter } from '../../../stores/router'
+import { hideModal } from '../../../stores/ui'
 import { validatePassword } from '../../../utils/validatePassword'
 
 import { PasswordField } from './PasswordField'
@@ -24,13 +25,18 @@ export const ChangePasswordForm = () => {
   const [isSubmitting, setIsSubmitting] = createSignal(false)
   const [validationErrors, setValidationErrors] = createSignal<ValidationErrors>({})
   const [newPassword, setNewPassword] = createSignal<string>()
+  const [isSuccess, setIsSuccess] = createSignal(false)
 
   const authFormRef: { current: HTMLFormElement } = { current: null }
 
   const handleSubmit = async (event: Event) => {
     event.preventDefault()
     setIsSubmitting(true)
-    // Change password logic
+    // Fake change password logic
+    setTimeout(() => {
+      setIsSubmitting(false)
+      setIsSuccess(true)
+    }, 1000)
   }
 
   const handlePasswordInput = (value) => {
@@ -60,43 +66,59 @@ export const ChangePasswordForm = () => {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      class={clsx(styles.authForm, styles.authFormForgetPassword)}
-      ref={(el) => (authFormRef.current = el)}
-    >
-      <div>
-        <h4>{t('Enter a new password')}</h4>
-        <div class={styles.authSubtitle}>
-          {t(
-            'Now you can enter a new password, it must contain at least 8 characters and not be the same as the previous password',
-          )}
-        </div>
+    <>
+      <Show when={!isSuccess()}>
+        <form
+          onSubmit={handleSubmit}
+          class={clsx(styles.authForm, styles.authFormForgetPassword)}
+          ref={(el) => (authFormRef.current = el)}
+        >
+          <div>
+            <h4>{t('Enter a new password')}</h4>
+            <div class={styles.authSubtitle}>
+              {t(
+                'Now you can enter a new password, it must contain at least 8 characters and not be the same as the previous password',
+              )}
+            </div>
 
-        <PasswordField error={validationErrors().password} value={(value) => handlePasswordInput(value)} />
-        <PasswordField
-          error={validationErrors().repeatPassword}
-          value={(value) => handlePasswordCopyInput(value)}
-        />
+            <PasswordField
+              error={validationErrors().password}
+              value={(value) => handlePasswordInput(value)}
+            />
+            <PasswordField
+              error={validationErrors().repeatPassword}
+              value={(value) => handlePasswordCopyInput(value)}
+            />
 
+            <div>
+              <button class={clsx('button', styles.submitButton)} disabled={isSubmitting()} type="submit">
+                {isSubmitting() ? '...' : t('Change password')}
+              </button>
+            </div>
+            <div class={styles.authControl}>
+              <span
+                class={styles.authLink}
+                onClick={() =>
+                  changeSearchParams({
+                    mode: 'login',
+                  })
+                }
+              >
+                {t('Cancel')}
+              </span>
+            </div>
+          </div>
+        </form>
+      </Show>
+      <Show when={isSuccess()}>
+        <div class={styles.title}>{t('Password updated!')}</div>
+        <div class={styles.text}>{t('You can now login using your new password')}</div>
         <div>
-          <button class={clsx('button', styles.submitButton)} disabled={isSubmitting()} type="submit">
-            {isSubmitting() ? '...' : t('Change password')}
+          <button class={clsx('button', styles.submitButton)} onClick={() => hideModal()}>
+            {t('Back to main page')}
           </button>
         </div>
-        <div class={styles.authControl}>
-          <span
-            class={styles.authLink}
-            onClick={() =>
-              changeSearchParams({
-                mode: 'login',
-              })
-            }
-          >
-            {t('Cancel')}
-          </span>
-        </div>
-      </div>
-    </form>
+      </Show>
+    </>
   )
 }
--- src/components/Nav/AuthModal/index.tsx
+++ src/components/Nav/AuthModal/index.tsx
@@ -26,7 +26,7 @@ const AUTH_MODAL_MODES: Record<AuthModalMode, Component> = {
 }
 
 export const AuthModal = () => {
-  let rootRef: HTMLDivElement
+  const rootRef: { current: HTMLDivElement } = { current: null }
   const { t } = useLocalize()
   const { searchParams } = useRouter<AuthModalSearchParams>()
 
@@ -38,17 +38,17 @@ export const AuthModal = () => {
 
   createEffect((oldMode) => {
     if (oldMode !== mode() && !isMobile()) {
-      rootRef?.querySelector('input')?.focus()
+      rootRef.current?.querySelector('input')?.focus()
     }
   }, null)
 
   return (
     <div
-      ref={rootRef}
+      ref={(el) => (rootRef.current = el)}
       class={clsx(styles.view, {
         row: !source,
+        [styles.signUp]: mode() === 'register' || mode() === 'confirm-email',
       })}
-      classList={{ [styles.signUp]: mode() === 'register' || mode() === 'confirm-email' }}
     >
       <Show when={!source}>
         <div class={clsx('col-md-12 d-none d-md-flex', styles.authImage)}>

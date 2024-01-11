417f2c66783d79eb0c987e74fbfd7feb2a0f0414
Untone <anton.rewin@gmail.com>
2024-01-11 10:02:36 +0300

    server-lodaing-fix

--- src/pages/index.page.server.ts
+++ src/pages/index.page.server.ts
@@ -1,12 +1,16 @@
 import type { PageProps } from './types'
 import type { PageContext } from '../renderer/types'
 
+import { PRERENDERED_ARTICLES_COUNT } from '../components/Views/Home'
 import { apiClient } from '../graphql/client/core'
 
 export const onBeforeRender = async (_pageContext: PageContext) => {
-  const allAuthors = await apiClient.getAllAuthors()
+  const homeShouts = await apiClient.getShouts({
+    filters: { published: true },
+    limit: PRERENDERED_ARTICLES_COUNT,
+  })
 
-  const pageProps: PageProps = { allAuthors, seo: { title: '' } }
+  const pageProps: PageProps = { homeShouts, seo: { title: '' } }
 
   return {
     pageContext: {

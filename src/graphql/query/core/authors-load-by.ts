35efc0b6a21e7c7ea537157e4620951793108f79
Untone <anton.rewin@gmail.com>
2024-01-30 19:11:57 +0300

    query fix

--- src/graphql/query/core/authors-load-by.ts
+++ src/graphql/query/core/authors-load-by.ts
@@ -1,7 +1,7 @@
 import { gql } from '@urql/core'
 
 export default gql`
-  query AuthorsAllQuery($by: AuthorsBy, $limit: Int, $offset: Int) {
+  query AuthorsAllQuery($by: AuthorsBy!, $limit: Int, $offset: Int) {
     load_authors_by(by: $by, limit: $limit, offset: $offset) {
       id
       slug

9b1f3c993cd57a3ff6989b62c3c6c666c74da6da
Untone <anton.rewin@gmail.com>
2024-01-31 22:06:29 +0300

    delete-fix

--- src/graphql/mutation/core/article-delete.ts
+++ src/graphql/mutation/core/article-delete.ts
@@ -1,8 +1,8 @@
 import { gql } from '@urql/core'
 
 export default gql`
-  mutation DeleteShoutMutation($shoutId: Int!) {
-    delete_shout(shout_id: $shoutId) {
+  mutation DeleteShoutMutation($shout_id: Int!) {
+    delete_shout(shout_id: $shout_id) {
       error
     }
   }

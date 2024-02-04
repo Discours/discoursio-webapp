e95558fb51f94507c39bad69ea740c13ae83cd5f
Untone <anton.rewin@gmail.com>
2024-02-04 16:25:23 +0300

    e2e

--- tests/basic-routes.spec.ts
+++ tests/basic-routes.spec.ts
@@ -1,30 +1,21 @@
 import { expect, test } from '@playwright/test'
 
+const baseHost = 'https://localhost:3000'
+
 const pagesTitles = {
-  'http://localhost:3000/': 'Дискурс',
-  'http://localhost:3000/feed': 'Дискурс: лента',
-  'http://localhost:3000/create': 'Дискурс',
-  'http://localhost:3000/about/donate': 'Дискурс: поддержка',
-  'http://localhost:3000/authors': 'Дискурс: авторы',
-  'http://localhost:3000/topics': 'Дискурс: темы',
-  'http://localhost:3000/inbox': 'Дискурс',
+  '/': /Дискурс/,
+  '/feed': /Дискурс/,
+  '/create': /Дискурс/,
+  '/about/donate': /Дискурс/,
+  '/authors': /Дискурс/,
+  '/topics': /Дискурс/,
+  '/inbox': /Дискурс/,
 }
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Дискурс/)
-})
-
-test('check some links', async ({ page }) => {
-  await page.goto('http://localhost:3000/feed')
-
-  await expect(page).toHaveTitle(/Дискурс/)
-
-  // Click the get started link.
-  // await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+Object.keys(pagesTitles).forEach((res: string) => {
+  test(`страница ${res}`, async ({ page }) => {
+    await page.goto(`${baseHost}${res}`)
+    const title = pagesTitles[res]
+    await expect(page).toHaveTitle(title)
+  })
 })

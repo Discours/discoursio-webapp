812dd02a6f4f13352b5fd77f609202d85b912567
Untone <anton.rewin@gmail.com>
2024-02-16 22:45:11 +0300

    build-fix-2

--- src/renderer/_default.page.client.tsx
+++ src/renderer/_default.page.client.tsx
@@ -23,13 +23,11 @@ export const render = async (pageContext: PageContextBuiltInClientWithClientRout
 
   Sentry.init({
     dsn: SENTRY_DSN,
-    integrations: [
-      Sentry.replayIntegration(),
-    ],
+    integrations: [Sentry.replayIntegration()],
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
+    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  })
 
   // eslint-disable-next-line import/no-named-as-default-member
   await i18next

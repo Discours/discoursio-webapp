264ebddeac6ab6fbc0fd5d94526d5f5118bd011d
ilya-bkv <i.yablokov@ccmp.me>
2024-02-16 16:39:41 +0300

    test

--- src/components/Views/AllAuthors/AllAuthors.tsx
+++ src/components/Views/AllAuthors/AllAuthors.tsx
@@ -79,99 +79,99 @@ export const AllAuthors = (props: Props) => {
       <Meta name="twitter:title" content={ogTitle} />
       <Meta name="twitter:description" content={description} />
       <Show when={props.isLoaded} fallback={<Loading />}>
-        {/*<div class="offset-md-5">*/}
-        {/*  <div class="row">*/}
-        {/*    <div class="col-lg-20 col-xl-18">*/}
-        {/*      <h1>{t('Authors')}</h1>*/}
-        {/*      <p>{t('Subscribe who you like to tune your personal feed')}</p>*/}
-        {/*      <ul class={clsx(styles.viewSwitcher, 'view-switcher')}>*/}
-        {/*        <li*/}
-        {/*          class={clsx({*/}
-        {/*            ['view-switcher__item--selected']: !searchParams().by || searchParams().by === 'shouts',*/}
-        {/*          })}*/}
-        {/*        >*/}
-        {/*          <a href="/authors?by=shouts">{t('By shouts')}</a>*/}
-        {/*        </li>*/}
-        {/*        <li*/}
-        {/*          class={clsx({*/}
-        {/*            ['view-switcher__item--selected']: searchParams().by === 'followers',*/}
-        {/*          })}*/}
-        {/*        >*/}
-        {/*          <a href="/authors?by=followers">{t('By popularity')}</a>*/}
-        {/*        </li>*/}
-        {/*        <li*/}
-        {/*          class={clsx({*/}
-        {/*            ['view-switcher__item--selected']: searchParams().by === 'name',*/}
-        {/*          })}*/}
-        {/*        >*/}
-        {/*          <a href="/authors?by=name">{t('By name')}</a>*/}
-        {/*        </li>*/}
-        {/*        <Show when={searchParams().by !== 'name'}>*/}
-        {/*          <li class="view-switcher__search">*/}
-        {/*            <SearchField onChange={(value) => setSearchQuery(value)} />*/}
-        {/*          </li>*/}
-        {/*        </Show>*/}
-        {/*      </ul>*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
+        <div class="offset-md-5">
+          <div class="row">
+            <div class="col-lg-20 col-xl-18">
+              <h1>{t('Authors')}</h1>
+              <p>{t('Subscribe who you like to tune your personal feed')}</p>
+              <ul class={clsx(styles.viewSwitcher, 'view-switcher')}>
+                <li
+                  class={clsx({
+                    ['view-switcher__item--selected']: !searchParams().by || searchParams().by === 'shouts',
+                  })}
+                >
+                  <a href="/authors?by=shouts">{t('By shouts')}</a>
+                </li>
+                <li
+                  class={clsx({
+                    ['view-switcher__item--selected']: searchParams().by === 'followers',
+                  })}
+                >
+                  <a href="/authors?by=followers">{t('By popularity')}</a>
+                </li>
+                <li
+                  class={clsx({
+                    ['view-switcher__item--selected']: searchParams().by === 'name',
+                  })}
+                >
+                  <a href="/authors?by=name">{t('By name')}</a>
+                </li>
+                <Show when={searchParams().by !== 'name'}>
+                  <li class="view-switcher__search">
+                    <SearchField onChange={(value) => setSearchQuery(value)} />
+                  </li>
+                </Show>
+              </ul>
+            </div>
+          </div>
 
-        {/*  <Show when={searchParams().by === 'name'}>*/}
-        {/*    <div class="row">*/}
-        {/*      <div class="col-lg-20 col-xl-18">*/}
-        {/*        <ul class={clsx('nodash', styles.alphabet)}>*/}
-        {/*          <For each={ALPHABET}>*/}
-        {/*            {(letter, index) => (*/}
-        {/*              <li>*/}
-        {/*                <Show when={letter in byLetter()} fallback={letter}>*/}
-        {/*                  <a*/}
-        {/*                    href={`/authors?by=name#letter-${index()}`}*/}
-        {/*                    onClick={(event) => {*/}
-        {/*                      event.preventDefault()*/}
-        {/*                      scrollHandler(`letter-${index()}`)*/}
-        {/*                    }}*/}
-        {/*                  >*/}
-        {/*                    {letter}*/}
-        {/*                  </a>*/}
-        {/*                </Show>*/}
-        {/*              </li>*/}
-        {/*            )}*/}
-        {/*          </For>*/}
-        {/*        </ul>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
+          <Show when={searchParams().by === 'name'}>
+            <div class="row">
+              <div class="col-lg-20 col-xl-18">
+                <ul class={clsx('nodash', styles.alphabet)}>
+                  <For each={ALPHABET}>
+                    {(letter, index) => (
+                      <li>
+                        <Show when={letter in byLetter()} fallback={letter}>
+                          <a
+                            href={`/authors?by=name#letter-${index()}`}
+                            onClick={(event) => {
+                              event.preventDefault()
+                              scrollHandler(`letter-${index()}`)
+                            }}
+                          >
+                            {letter}
+                          </a>
+                        </Show>
+                      </li>
+                    )}
+                  </For>
+                </ul>
+              </div>
+            </div>
 
-        {/*    <For each={sortedKeys()}>*/}
-        {/*      {(letter) => (*/}
-        {/*        <div class={clsx(styles.group, 'group')}>*/}
-        {/*          <h2 id={`letter-${ALPHABET.indexOf(letter)}`}>{letter}</h2>*/}
-        {/*          <div class="container">*/}
-        {/*            <div class="row">*/}
-        {/*              <div class="col-lg-20">*/}
-        {/*                <div class="row">*/}
-        {/*                  <For each={byLetter()[letter]}>*/}
-        {/*                    {(author) => (*/}
-        {/*                      <div class={clsx(styles.topic, 'topic col-sm-12 col-md-8')}>*/}
-        {/*                        <div class="topic-title">*/}
-        {/*                          <a href={`/author/${author.slug}`}>{translateAuthor(author, lang())}</a>*/}
-        {/*                          <Show when={author.stat}>*/}
-        {/*                            <span class={styles.articlesCounter}>{author.stat.shouts}</span>*/}
-        {/*                          </Show>*/}
-        {/*                        </div>*/}
-        {/*                      </div>*/}
-        {/*                    )}*/}
-        {/*                  </For>*/}
-        {/*                </div>*/}
-        {/*              </div>*/}
-        {/*            </div>*/}
-        {/*          </div>*/}
-        {/*        </div>*/}
-        {/*      )}*/}
-        {/*    </For>*/}
-        {/*  </Show>*/}
-        {/*  <Show when={searchParams().by !== 'name' && props.isLoaded}>*/}
-        {/*    <AuthorsList query={searchParams().by === 'shouts' ? 'shouts' : 'followers'} />*/}
-        {/*  </Show>*/}
-        {/*</div>*/}
+            <For each={sortedKeys()}>
+              {(letter) => (
+                <div class={clsx(styles.group, 'group')}>
+                  <h2 id={`letter-${ALPHABET.indexOf(letter)}`}>{letter}</h2>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-20">
+                        <div class="row">
+                          <For each={byLetter()[letter]}>
+                            {(author) => (
+                              <div class={clsx(styles.topic, 'topic col-sm-12 col-md-8')}>
+                                <div class="topic-title">
+                                  <a href={`/author/${author.slug}`}>{translateAuthor(author, lang())}</a>
+                                  <Show when={author.stat}>
+                                    <span class={styles.articlesCounter}>{author.stat.shouts}</span>
+                                  </Show>
+                                </div>
+                              </div>
+                            )}
+                          </For>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </For>
+          </Show>
+          <Show when={searchParams().by !== 'name' && props.isLoaded} fallback={<Loading/>}>
+            <AuthorsList query={searchParams().by === 'shouts' ? 'shouts' : 'followers'} />
+          </Show>
+        </div>
       </Show>
     </div>
   )

ab4d6b8be939375b2a0ece8b075be04c93ad6509
ilya-bkv <i.yablokov@ccmp.me>
2024-02-22 10:18:25 +0300

    Load Authors by btn click

--- src/components/AuthorsList/AuthorsList.tsx
+++ src/components/AuthorsList/AuthorsList.tsx
@@ -1,13 +1,13 @@
-import { createInfiniteScroll } from '@solid-primitives/pagination'
 import { clsx } from 'clsx'
-import { For, Show, createEffect } from 'solid-js'
+import { createSignal, For, Show, createEffect } from 'solid-js'
 import { useFollowing } from '../../context/following'
 import { apiClient } from '../../graphql/client/core'
-import type { Author } from '../../graphql/schema/core.gen'
 import { setAuthorsByFollowers, setAuthorsByShouts, useAuthorsStore } from '../../stores/zine/authors'
 import { AuthorBadge } from '../Author/AuthorBadge'
 import { InlineLoader } from '../InlineLoader'
 import styles from './AuthorsList.module.scss'
+import { Button } from '../_shared/Button'
+import { useLocalize } from '../../context/localize'
 
 type Props = {
   class?: string
@@ -16,73 +16,70 @@ type Props = {
 
 const PAGE_SIZE = 20
 export const AuthorsList = (props: Props) => {
+  const { t } = useLocalize()
   const { isOwnerSubscribed } = useFollowing()
+  const [loading, setLoading] = createSignal(false)
+  const [currentPage, setCurrentPage] = createSignal({ shouts: 0, followers: 0 })
   const { authorsByShouts, authorsByFollowers } = useAuthorsStore()
 
-  const fetchAuthors = async (page: number) => {
-    const scrollTop = window.scrollY
-    const getPage = () => {
-      if (props.query === 'shouts') {
-        return authorsByShouts().length / PAGE_SIZE
-      }
-      if (props.query === 'followers') {
-        return authorsByFollowers().length / PAGE_SIZE
-      }
-      return page
-    }
-
+  const fetchAuthors = async (queryType: 'shouts' | 'followers', page: number) => {
+    setLoading(true)
+    const offset = PAGE_SIZE * page
     const result = await apiClient.loadAuthorsBy({
-      by: { order: props.query },
+      by: { order: queryType },
       limit: PAGE_SIZE,
-      offset: PAGE_SIZE * getPage(),
+      offset: offset,
     })
 
-    if (props.query === 'shouts') {
+    if (queryType === 'shouts') {
       setAuthorsByShouts((prev) => [...prev, ...result])
-    } else if (props.query === 'followers') {
+    } else {
       setAuthorsByFollowers((prev) => [...prev, ...result])
     }
-
-    requestAnimationFrame(() => {
-      window.scrollTo(0, scrollTop)
-    })
-
+    setLoading(false)
     return result
   }
 
-  const [pages, setEl, { setPage, setPages, end, setEnd }] = createInfiniteScroll(fetchAuthors)
+  const loadMoreAuthors = () => {
+    const queryType = props.query
+    const nextPage = currentPage()[queryType] + 1
+    fetchAuthors(queryType, nextPage).then(() => setCurrentPage({ ...currentPage(), [queryType]: nextPage }))
+  }
 
   createEffect(() => {
-    const initialData = props.query === 'shouts' ? authorsByShouts() : authorsByFollowers()
-    setPage(0)
-    setPages(initialData)
-    setEnd(false)
+    const queryType = props.query
+    if (currentPage()[queryType] === 0 && (authorsByShouts().length === 0 || authorsByFollowers().length === 0)) {
+      loadMoreAuthors()
+    }
   })
 
+  const authorsList = () => (props.query === 'shouts' ? authorsByShouts() : authorsByFollowers())
+
   return (
-    <Show when={pages()}>
-      <div class={clsx(styles.AuthorsList, props.class)}>
-        <For each={pages()}>
-          {(author: Author) => (
-            <div class="row">
-              <div class="col-lg-20 col-xl-18">
-                <AuthorBadge
-                  author={author}
-                  isFollowed={{
-                    loaded: Boolean(pages().length),
-                    value: isOwnerSubscribed(author.id),
-                  }}
-                />
-              </div>
+    <div class={clsx(styles.AuthorsList, props.class)}>
+      <For each={authorsList()}>
+        {(author) => (
+          <div class="row">
+            <div class="col-lg-20 col-xl-18">
+              <AuthorBadge
+                author={author}
+                isFollowed={{
+                  loaded: !loading(),
+                  value: isOwnerSubscribed(author.id),
+                }}
+              />
             </div>
-          )}
-        </For>
-        <Show when={!end()}>
-          <div ref={setEl as (e: HTMLDivElement) => void}>
-            <InlineLoader />
           </div>
+        )}
+      </For>
+      <div class={styles.action}>
+        <Show when={!loading()}>
+          <Button value={t('Load more')} onClick={loadMoreAuthors} />
+        </Show>
+        <Show when={loading()}>
+          <InlineLoader />
         </Show>
       </div>
-    </Show>
+    </div>
   )
 }

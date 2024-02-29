d0be8ffb6a0a6af5d9fbf3fa4bd71cbdb3e07c4c
Untone <anton.rewin@gmail.com>
2024-03-01 00:11:59 +0300

    minor

--- src/pages/allAuthors.page.server.ts
+++ src/pages/allAuthors.page.server.ts
@@ -1,13 +1,21 @@
 import type { PageContext } from '../renderer/types'
 import type { PageProps } from './types'
 
+import { PAGE_SIZE } from '../components/Views/AllTopics/AllTopics'
 import { apiClient } from '../graphql/client/core'
-import { PAGE_SIZE } from "../components/Views/AllTopics/AllTopics";
 
 export const onBeforeRender = async (_pageContext: PageContext) => {
   const allAuthors = await apiClient.getAllAuthors()
-  const topWritingAuthors = await apiClient.loadAuthorsBy({ by: { order: 'shouts' }, limit: PAGE_SIZE})
-  const topFollowedAuthors = await apiClient.loadAuthorsBy({ by: { order: 'followers' }, limit: PAGE_SIZE})
+  const topWritingAuthors = await apiClient.loadAuthorsBy({
+    by: { order: 'shouts' },
+    limit: PAGE_SIZE,
+    offset: 0,
+  })
+  const topFollowedAuthors = await apiClient.loadAuthorsBy({
+    by: { order: 'followers' },
+    limit: PAGE_SIZE,
+    offset: 0,
+  })
   const pageProps: PageProps = { allAuthors, seo: { title: '' }, topWritingAuthors, topFollowedAuthors }
 
   return {

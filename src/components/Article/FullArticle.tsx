4b0a0db7d59cec24b0e8e5d3a1ee95e11b37720b
ilya-bkv <i.yablokov@ccmp.me>
2024-04-08 09:58:32 +0300

    Add gallery description

--- src/components/Article/FullArticle.tsx
+++ src/components/Article/FullArticle.tsx
@@ -329,7 +329,7 @@ export const FullArticle = (props: Props) => {
     width: 1200,
   })
 
-  const description = getDescription(props.article.description || body())
+  const description = getDescription(props.article.description || body() || props.article.title)
   const ogTitle = props.article.title
   const keywords = getKeywords(props.article)
   const shareUrl = getShareUrl({ pathname: `/${props.article.slug}` })

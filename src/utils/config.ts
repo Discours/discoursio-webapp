ec70a078ad8ebb15a77f550e6d8083117daeedb6
Untone <anton.rewin@gmail.com>
2024-04-10 16:37:09 +0300

    connected

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,6 +5,7 @@ export const cdnUrl = 'https://cdn.discours.io'
 export const thumborUrl = import.meta.env.PUBLIC_THUMBOR_URL || defaultThumborUrl
 
 export const SENTRY_DSN = import.meta.env.PUBLIC_SENTRY_DSN || ''
+export const GLITCHTIP_DSN = import.meta.env.PUBLIC_GLITCHTIP_DSN || ''
 
 const defaultSearchUrl = 'https://search.discours.io'
 export const searchUrl = import.meta.env.PUBLIC_SEARCH_URL || defaultSearchUrl

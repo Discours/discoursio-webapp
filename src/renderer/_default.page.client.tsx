e1cfa376c5a2ca875676ecbcbaef33c54fa9e5fa
Untone <anton.rewin@gmail.com>
2024-04-10 17:02:58 +0300

    fmt

--- src/renderer/_default.page.client.tsx
+++ src/renderer/_default.page.client.tsx
@@ -22,13 +22,13 @@ export const render = async (pageContext: PageContextBuiltInClientWithClientRout
   initRouter(pathname, searchParams)
 
   Sentry.init({
-      dsn: GLITCHTIP_DSN,
-      tracesSampleRate: 0.01,
-      integrations: [Sentry.replayIntegration()],
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-    });
+    dsn: GLITCHTIP_DSN,
+    tracesSampleRate: 0.01,
+    integrations: [Sentry.replayIntegration()],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  })
 
   // eslint-disable-next-line import/no-named-as-default-member
   await i18next

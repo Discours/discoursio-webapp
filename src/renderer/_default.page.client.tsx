f3d55b635ae785efac97ce146a2543b22b698b8a
Untone <anton.rewin@gmail.com>
2024-04-10 08:56:27 +0300

    glitchtip

--- src/renderer/_default.page.client.tsx
+++ src/renderer/_default.page.client.tsx
@@ -1,33 +1,35 @@
-import type { PageContextBuiltInClientWithClientRouting } from 'vike/types'
-import type { PageContext } from './types'
+import type { PageContextBuiltInClientWithClientRouting } from "vike/types";
+import type { PageContext } from "./types";
 
-import * as Sentry from '@sentry/browser'
-import i18next from 'i18next'
-import HttpApi from 'i18next-http-backend'
-import ICU from 'i18next-icu'
-import { hydrate } from 'solid-js/web'
+import * as Sentry from "@sentry/browser";
+import i18next from "i18next";
+import HttpApi from "i18next-http-backend";
+import ICU from "i18next-icu";
+import { hydrate } from "solid-js/web";
 
-import { App } from '../components/App'
-import { initRouter } from '../stores/router'
-import { SENTRY_DSN } from '../utils/config'
-import { resolveHydrationPromise } from '../utils/hydrationPromise'
+import { App } from "../components/App";
+import { initRouter } from "../stores/router";
+import { GLITCHTIP_DSN } from "../utils/config";
+import { resolveHydrationPromise } from "../utils/hydrationPromise";
 
-let layoutReady = false
+let layoutReady = false;
 
-export const render = async (pageContext: PageContextBuiltInClientWithClientRouting & PageContext) => {
-  const { lng, pageProps, is404 } = pageContext
+export const render = async (
+  pageContext: PageContextBuiltInClientWithClientRouting & PageContext,
+) => {
+  const { lng, pageProps, is404 } = pageContext;
 
-  const { pathname, search } = window.location
-  const searchParams = Object.fromEntries(new URLSearchParams(search))
-  initRouter(pathname, searchParams)
+  const { pathname, search } = window.location;
+  const searchParams = Object.fromEntries(new URLSearchParams(search));
+  initRouter(pathname, searchParams);
 
   Sentry.init({
-    dsn: SENTRY_DSN,
+    dsn: GLITCHTIP_DSN,
     integrations: [Sentry.replayIntegration()],
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  })
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
 
   // eslint-disable-next-line import/no-named-as-default-member
   await i18next
@@ -35,27 +37,29 @@ export const render = async (pageContext: PageContextBuiltInClientWithClientRout
     .use(ICU)
     .init({
       // debug: true,
-      supportedLngs: ['ru', 'en'],
+      supportedLngs: ["ru", "en"],
       fallbackLng: lng,
       lng,
-      load: 'languageOnly'
-    })
+      load: "languageOnly",
+    });
 
-  const isIOSorMacOSorAndroid = /iphone|ipad|ipod|macintosh|android/i.test(navigator.userAgent)
+  const isIOSorMacOSorAndroid = /iphone|ipad|ipod|macintosh|android/i.test(
+    navigator.userAgent,
+  );
 
   if (!isIOSorMacOSorAndroid) {
-    const htmlEl = document.querySelector('html')
-    htmlEl.dataset.customScroll = 'on'
+    const htmlEl = document.querySelector("html");
+    htmlEl.dataset.customScroll = "on";
   }
 
-  const content = document.querySelector('#root')
+  const content = document.querySelector("#root");
 
   if (!layoutReady) {
-    hydrate(() => <App {...pageProps} is404={is404} />, content)
-    layoutReady = true
+    hydrate(() => <App {...pageProps} is404={is404} />, content);
+    layoutReady = true;
   }
-}
+};
 
 export const onHydrationEnd = () => {
-  resolveHydrationPromise()
-}
+  resolveHydrationPromise();
+};

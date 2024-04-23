b5d4e6b015bb7746cf8447a8b190c1f296295a87
dog <arkadyrakovsky@gmail.com>
2024-04-23 15:51:43 +0300

    fix falsy active header

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,92 +1,92 @@
-import { clsx } from "clsx";
-import { For, Show, createEffect, createSignal, on, onCleanup, onMount } from "solid-js";
-import { debounce, throttle } from "throttle-debounce";
+import { clsx } from 'clsx'
+import { For, Show, createEffect, createSignal, on, onCleanup, onMount } from 'solid-js'
+import { debounce, throttle } from 'throttle-debounce'
 
-import { useLocalize } from "../../context/localize";
-import { DEFAULT_HEADER_OFFSET } from "../../stores/router";
-import { isDesktop } from "../../utils/media-query";
-import { Icon } from "../_shared/Icon";
+import { useLocalize } from '../../context/localize'
+import { DEFAULT_HEADER_OFFSET } from '../../stores/router'
+import { isDesktop } from '../../utils/media-query'
+import { Icon } from '../_shared/Icon'
 
-import styles from "./TableOfContents.module.scss";
+import styles from './TableOfContents.module.scss'
 
 interface Props {
-  variant: "article" | "editor";
-  parentSelector: string;
-  body: string;
+  variant: 'article' | 'editor'
+  parentSelector: string
+  body: string
 }
 
 const isInViewport = (el: Element): boolean => {
-  const rect = el.getBoundingClientRect();
-  return rect.top <= DEFAULT_HEADER_OFFSET + 24; // default offset + 1.5em (default header margin-top)
-};
+  const rect = el.getBoundingClientRect()
+  return rect.top <= DEFAULT_HEADER_OFFSET + 24 // default offset + 1.5em (default header margin-top)
+}
 const scrollToHeader = (element) => {
   window.scrollTo({
-    behavior: "smooth",
+    behavior: 'smooth',
     top:
       element.getBoundingClientRect().top -
       document.body.getBoundingClientRect().top -
       DEFAULT_HEADER_OFFSET,
-  });
-};
+  })
+}
 
 export const TableOfContents = (props: Props) => {
-  const { t } = useLocalize();
+  const { t } = useLocalize()
 
-  const [headings, setHeadings] = createSignal<HTMLElement[]>([]);
-  const [areHeadingsLoaded, setAreHeadingsLoaded] = createSignal<boolean>(false);
-  const [activeHeaderIndex, setActiveHeaderIndex] = createSignal<number>(-1);
-  const [isVisible, setIsVisible] = createSignal<boolean>(props.variant === "article");
+  const [headings, setHeadings] = createSignal<HTMLElement[]>([])
+  const [areHeadingsLoaded, setAreHeadingsLoaded] = createSignal<boolean>(false)
+  const [activeHeaderIndex, setActiveHeaderIndex] = createSignal<number>(-1)
+  const [isVisible, setIsVisible] = createSignal<boolean>(props.variant === 'article')
   const toggleIsVisible = () => {
-    setIsVisible((visible) => !visible);
-  };
+    setIsVisible((visible) => !visible)
+  }
 
-  setIsVisible(isDesktop());
+  setIsVisible(isDesktop())
 
   const updateHeadings = () => {
     setHeadings(
       // eslint-disable-next-line unicorn/prefer-spread
       Array.from(
-        document.querySelector(props.parentSelector).querySelectorAll<HTMLElement>("h1, h2, h3, h4"),
+        document.querySelector(props.parentSelector).querySelectorAll<HTMLElement>('h1, h2, h3, h4'),
       ),
-    );
-    setAreHeadingsLoaded(true);
-  };
+    )
+    setAreHeadingsLoaded(true)
+  }
 
-  const debouncedUpdateHeadings = debounce(500, updateHeadings);
+  const debouncedUpdateHeadings = debounce(500, updateHeadings)
 
   const updateActiveHeader = throttle(50, () => {
-    const newActiveIndex = headings().findLastIndex((heading) => isInViewport(heading));
-    setActiveHeaderIndex(newActiveIndex);
-  });
+    const newActiveIndex = headings().findLastIndex((heading) => isInViewport(heading))
+    setActiveHeaderIndex(newActiveIndex)
+  })
 
   createEffect(
     on(
       () => props.body,
       () => debouncedUpdateHeadings(),
     ),
-  );
+  )
 
   onMount(() => {
-    window.addEventListener("scroll", updateActiveHeader);
-    onCleanup(() => window.removeEventListener("scroll", updateActiveHeader));
-  });
+    window.addEventListener('scroll', updateActiveHeader)
+    onCleanup(() => window.removeEventListener('scroll', updateActiveHeader))
+  })
 
   return (
     <Show
       when={
-        areHeadingsLoaded() && (props.variant === "article" ? headings().length > 2 : headings().length > 1)
+        areHeadingsLoaded() && (props.variant === 'article' ? headings().length > 2 : headings().length > 1)
       }
     >
       <div
         class={clsx(styles.TableOfContentsFixedWrapper, {
-          [styles.TableOfContentsFixedWrapperLefted]: props.variant === "editor",
+          [styles.TableOfContentsFixedWrapperLefted]: props.variant === 'editor',
         })}
       >
         <div class={styles.TableOfContentsContainer}>
           <Show when={isVisible()}>
             <div class={styles.TableOfContentsContainerInner}>
               <div class={styles.TableOfContentsHeader}>
-                <p class={styles.TableOfContentsHeading}>{t("contents")}</p>
+                <p class={styles.TableOfContentsHeading}>{t('contents')}</p>
               </div>
               <ul class={styles.TableOfContentsHeadingsList}>
                 <For each={headings()}>
@@ -94,14 +94,14 @@ export const TableOfContents = (props: Props) => {
                     <li>
                       <button
                         class={clsx(styles.TableOfContentsHeadingsItem, {
-                          [styles.TableOfContentsHeadingsItemH3]: h.nodeName === "H3",
-                          [styles.TableOfContentsHeadingsItemH4]: h.nodeName === "H4",
+                          [styles.TableOfContentsHeadingsItemH3]: h.nodeName === 'H3',
+                          [styles.TableOfContentsHeadingsItemH4]: h.nodeName === 'H4',
                           [styles.active]: index() === activeHeaderIndex(),
                         })}
                         innerHTML={h.textContent}
                         onClick={(e) => {
-                          e.preventDefault();
-                          scrollToHeader(h);
+                          e.preventDefault()
+                          scrollToHeader(h)
                         }}
                       />
                     </li>
@@ -115,18 +115,18 @@ export const TableOfContents = (props: Props) => {
             class={clsx(
               styles.TableOfContentsPrimaryButton,
               {
-                [styles.TableOfContentsPrimaryButtonLefted]: props.variant === "editor" && !isVisible(),
+                [styles.TableOfContentsPrimaryButtonLefted]: props.variant === 'editor' && !isVisible(),
               },
-              "d-none d-xl-block",
+              'd-none d-xl-block',
             )}
             onClick={(e) => {
-              e.preventDefault();
-              toggleIsVisible();
+              e.preventDefault()
+              toggleIsVisible()
             }}
-            title={isVisible() ? t("Hide table of contents") : t("Show table of contents")}
+            title={isVisible() ? t('Hide table of contents') : t('Show table of contents')}
           >
             <Show when={isVisible()} fallback={<Icon name="show-table-of-contents" class="icon" />}>
-              {props.variant === "editor" ? (
+              {props.variant === 'editor' ? (
                 <Icon name="hide-table-of-contents" class="icon" />
               ) : (
                 <Icon name="hide-table-of-contents-2" class="icon" />
@@ -136,12 +136,12 @@ export const TableOfContents = (props: Props) => {
 
           <Show when={isVisible()}>
             <button
-              class={clsx(styles.TableOfContentsCloseButton, "d-xl-none")}
+              class={clsx(styles.TableOfContentsCloseButton, 'd-xl-none')}
               onClick={(e) => {
-                e.preventDefault();
-                toggleIsVisible();
+                e.preventDefault()
+                toggleIsVisible()
               }}
-              title={isVisible() ? t("Hide table of contents") : t("Show table of contents")}
+              title={isVisible() ? t('Hide table of contents') : t('Show table of contents')}
             >
               <Icon name="close-white" class="icon" />
             </button>
@@ -153,20 +153,20 @@ export const TableOfContents = (props: Props) => {
             class={clsx(
               styles.TableOfContentsPrimaryButton,
               {
-                [styles.TableOfContentsPrimaryButtonLefted]: props.variant === "editor" && !isVisible(),
+                [styles.TableOfContentsPrimaryButtonLefted]: props.variant === 'editor' && !isVisible(),
               },
-              "d-xl-none",
+              'd-xl-none',
             )}
             onClick={(e) => {
-              e.preventDefault();
-              toggleIsVisible();
+              e.preventDefault()
+              toggleIsVisible()
             }}
-            title={isVisible() ? t("Hide table of contents") : t("Show table of contents")}
+            title={isVisible() ? t('Hide table of contents') : t('Show table of contents')}
           >
             <Icon name="hide-table-of-contents-2" class="icon" />
           </button>
         </Show>
       </div>
     </Show>
-  );
-};
+  )
+}

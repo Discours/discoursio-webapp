28022607e314504eb2d1372dc30d1efc2c260931
Untone <anton.rewin@gmail.com>
2024-05-06 13:46:00 +0300

    topics-fix-2

--- src/utils/getRandomTopicsFromArray.ts
+++ src/utils/getRandomTopicsFromArray.ts
@@ -2,6 +2,7 @@ import { RANDOM_TOPICS_COUNT } from '../components/Views/Home'
 import { Topic } from '../graphql/schema/core.gen'
 
 export const getRandomTopicsFromArray = (topics: Topic[], count: number = RANDOM_TOPICS_COUNT): Topic[] => {
-  const shuffledTopics = [...(topics || [])].sort(() => 0.5 - Math.random())
+  if (!topics) return []
+  const shuffledTopics = [...topics].sort(() => 0.5 - Math.random())
   return shuffledTopics.slice(0, count)
 }

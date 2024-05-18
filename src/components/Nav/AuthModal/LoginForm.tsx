60664581bdb3f7154ef165305850657454a2b199
Untone <anton.rewin@gmail.com>
2024-05-18 20:50:27 +0300

    logfix

--- src/components/Nav/AuthModal/LoginForm.tsx
+++ src/components/Nav/AuthModal/LoginForm.tsx
@@ -95,8 +95,8 @@ export const LoginForm = () => {
 
     try {
       const { errors } = await signIn({ email: email(), password: password() })
-      console.error('[signIn errors]', errors)
       if (errors?.length > 0) {
+        console.debug('[signIn] errors:', errors)
         if (
           errors.some(
             (error) =>

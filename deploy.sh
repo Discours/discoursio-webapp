d2771ac2a4716eb3511a5754ed64266fc0b013d9
Untone <anton.rewin@gmail.com>
2024-07-01 16:57:30 +0300

    vercel-adapt

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
-if [
-    "$(git rev-parse --abbrev-ref HEAD)" = "router-upgrade"
-  ] || [
-    "$(git rev-parse --abbrev-ref HEAD)" = "feature/rating"
-  ];
-then exit 1;
-else exit 0;
+if [ "$VERCEL_GIT_COMMIT_REF" = "router-upgrade" ] || [ "$VERCEL_GIT_COMMIT_REF" = "feature/rating" ]; then
+  echo "Building on solid start"
+  exit 1
+else
+  echo "Not on solid start"
+  exit 0
 fi

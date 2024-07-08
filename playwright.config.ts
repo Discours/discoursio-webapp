3bfa4adf6855bcea1eef733b3632974ada32661a
Stepan Vladovskiy <stepan.vladovskiy@gmail.com>
2024-07-08 10:24:07 +0000

    fix: playwright config with baseURL env bar.

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://localhost:3000',
+    baseURL: process.env.BASE_URL || 'https://localhost:3000',
     /* Headless */
     headless: true,
     /* Ignode SSL certificates */

87dd66ad4bc8c01c040fe2a3bfd8b07bfb5a8d71
Untone <anton.rewin@gmail.com>
2024-07-23 17:00:33 +0300

    biomepass

--- src/components/Article/CommentsTree.tsx
+++ src/components/Article/CommentsTree.tsx
@@ -142,7 +142,11 @@ export const CommentsTree = (props: Props) => {
       <LoadMoreWrapper
         loadFunction={loadMoreComments}
         pageSize={COMMENTS_PER_PAGE}
-        hidden={props.shout?.stat?.commented === 0 || commentsLoading() || comments().length >= (props.shout?.stat?.commented || 0)}
+        hidden={
+          props.shout?.stat?.commented === 0 ||
+          commentsLoading() ||
+          comments().length >= (props.shout?.stat?.commented || 0)
+        }
       >
         <ul class={styles.comments}>
           <For each={sortedComments().filter((r) => !r.reply_to)}>

b868c7282d7ba148494e527419dbdd072046511a
Untone <anton.rewin@gmail.com>
2024-09-03 19:22:33 +0300

    postmerge

--- app.config.ts
+++ app.config.ts
@@ -35,16 +35,6 @@ export default defineConfig({
   },
   devOverlay: true,
   vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'icons': ['./src/components/_shared/Icon/Icon.tsx'],
-            'localize': ['./src/context/localize.tsx']
-          }
-        }
-      }
-    },
     envPrefix: 'PUBLIC_',
     plugins: [!isVercel && mkcert(), nodePolyfills(polyfillOptions), sassDts()],
     css: {

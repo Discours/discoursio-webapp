cd39b7b8066970b581a5c0b02b1f4c5373ee8b49
Untone <anton.rewin@gmail.com>
2024-10-09 18:55:45 +0300

    unfix

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,5 +1,6 @@
 import { clsx } from 'clsx'
-import { Show, createEffect } from 'solid-js'
+import { Show } from 'solid-js'
+import { Transition } from 'solid-transition-group'
 
 import { useSnackbar } from '~/context/ui'
 import { Icon } from '../_shared/Icon'
@@ -9,33 +10,29 @@ import styles from './Snackbar.module.scss'
 
 export const Snackbar = () => {
   const { snackbarMessage } = useSnackbar()
-  let snackbarRef: HTMLDivElement | undefined
-
-  createEffect(() => {
-    if (snackbarMessage()?.body) {
-      snackbarRef?.classList.add(styles.show)
-    } else {
-      snackbarRef?.classList.remove(styles.show)
-    }
-  })
 
   return (
     <div
-      ref={snackbarRef}
       class={clsx(styles.snackbar, {
         [styles.error]: snackbarMessage()?.type === 'error',
         [styles.success]: snackbarMessage()?.type === 'success'
       })}
     >
       <ShowOnlyOnClient>
-        <Show when={snackbarMessage()?.body}>
-          <div class={styles.content}>
-            <Show when={snackbarMessage()?.type === 'success'}>
-              <Icon name="check-success" class={styles.icon} />
-            </Show>
-            {snackbarMessage()?.body || ''}
-          </div>
-        </Show>
+        <Transition
+          enterClass={styles.enter}
+          exitToClass={styles.exitTo}
+          onExit={(_el, done) => setTimeout(() => done(), 300)}
+        >
+          <Show when={snackbarMessage()?.body}>
+            <div class={styles.content}>
+              <Show when={snackbarMessage()?.type === 'success'}>
+                <Icon name="check-success" class={styles.icon} />
+              </Show>
+              {snackbarMessage()?.body || ''}
+            </div>
+          </Show>
+        </Transition>
       </ShowOnlyOnClient>
     </div>
   )

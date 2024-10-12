33a60a58b22c453dc30d3182d5f9fa46c90ae02a
Stepan Vladovskiy <stepan.vladovskiy@gmail.com>
2024-10-12 19:58:51 +0000

    fix: modal reopening issue folowers. monitroing URL changes

--- src/components/_shared/FollowingCounters/FollowingCounters.tsx
+++ src/components/_shared/FollowingCounters/FollowingCounters.tsx
@@ -1,7 +1,8 @@
-import { For, Show, createMemo } from 'solid-js'
-import { A } from '@solidjs/router'
+import { For, Show, createMemo, createEffect } from 'solid-js'
+import { A, useLocation } from '@solidjs/router'
 
 import { useLocalize } from '~/context/localize'
+import { useUI } from '~/context/ui'
 
 import { Author, Topic } from '~/graphql/schema/core.gen'
 import { Userpic } from '../../Author/Userpic'
@@ -17,21 +18,18 @@ type Props = {
   authorsAmount?: number
   topics?: Topic[]
   topicsAmount?: number
-  onClose?: () => void // Add onClose prop to handle modal close
 }
 
 const UserpicList = (props: { items: Array<Author | Topic>; onClose?: () => void }) => (
   <div class={styles.subscribersList}>
     <For each={props.items.slice(0, 3)}>
       {(item) => (
-        <A href={`/${'name' in item ? item.name : 'title' in item ? item.title : ''}`} onClick={props.onClose}>
           <Userpic
             size="XS"
             name={'name' in item ? item.name || '' : 'title' in item ? item.title || '' : ''}
             userpic={item.pic || ''}
             class={styles.subscribersItem}
           />
-        </A>
       )}
     </For>
   </div>
@@ -43,31 +41,44 @@ const Counter = (props: { count: number; label: string }) => (
 
 export const FollowingCounters = (props: Props) => {
   const { t } = useLocalize()
+  const { hideModal, showModal } = useUI()
+  const location = useLocation()
 
   const getFollowersCount = createMemo(() => props.followersAmount || props.followers?.length || 0)
   const getFollowingCount = createMemo(() => props.followingAmount || props.following?.length || 0)
   const getAuthorsCount = createMemo(() => props.authorsAmount || props.authors?.length || 0)
   const getTopicsCount = createMemo(() => props.topicsAmount || props.topics?.length || 0)
 
+  // Monitor URL changes to control modal state
+  createEffect(() => {
+    const searchParams = new URLSearchParams(location.search)
+    const modalParam = searchParams.get('m')
+    if (modalParam === 'followers') {
+      showModal('followers')
+    } else {
+      hideModal()
+    }
+  })
+
   return (
     <>
       <A href="?m=followers" class={styles.subscribers}>
         <Show when={getFollowersCount() > 0}>
-          <UserpicList items={props.followers || []} onClose={props.onClose} />
+          <UserpicList items={props.followers || []} onClose={() => hideModal()} />
         </Show>
         <Counter count={getFollowersCount()} label={t('some followers', { count: getFollowersCount() })} />
       </A>
 
       <A href="?m=following" class={styles.subscribers}>
         <Show when={getFollowingCount() > 0}>
-          <UserpicList items={props.following || []} onClose={props.onClose} />
+          <UserpicList items={props.following || []} onClose={() => hideModal()} />
         </Show>
         <Show
           when={getFollowingCount() > 0}
           fallback={
             <>
               <Show when={getAuthorsCount() > 0}>
-                <UserpicList items={props.authors || []} onClose={props.onClose} />
+                <UserpicList items={props.authors || []} onClose={() => hideModal()} />
                 <Counter
                   count={getAuthorsCount()}
                   label={t('some authors', { count: getAuthorsCount() })}

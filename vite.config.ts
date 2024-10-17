3ff66c5766e8c8b7acafaf15163301fd760ec469
Untone <anton.rewin@gmail.com>
2024-10-17 13:09:25 +0300

    sass-updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // biome-ignore lint/correctness/noNodejsModules: build
 import path from 'node:path'
 import dotenv from 'dotenv'
-import { CSSOptions, defineConfig } from 'vite'
+import { CSSOptions, defineConfig, createLogger, LoggerOptions, LogLevel } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import { PolyfillOptions, nodePolyfills } from 'vite-plugin-node-polyfills'
 import sassDts from 'vite-plugin-sass-dts'
@@ -11,6 +11,15 @@ dotenv.config()
 
 export const isDev = process.env.NODE_ENV !== 'production'
 console.log(`[vite.config] ${process.env.NODE_ENV} mode`)
+
+const customLogger = createLogger('debug' as LogLevel, {
+  warn: (message: string, options: LoggerOptions) => {
+    if (message.includes('Future global-builtin deprecation is not yet active')) {
+      return // Игнорируем это конкретное предупреждение
+    }
+    console.warn(message, options)
+  }
+} as LoggerOptions)
 
 const polyfillOptions = {
   include: ['path', 'stream', 'util'],
@@ -34,12 +43,19 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['mixed-decls', 'legacy-js-api'],
+        api: 'modern-compiler',
+        quietDeps: true,
+        silenceDeprecations: [
+          'mixed-decls', 
+          'legacy-js-api', 
+          'global-builtin'
+        ],
         additionalData: (content: string) => `@use '~/styles/global' as *;\n${content}`,
         includePaths: ['./public', './src/styles', './node_modules']
       }
     } as CSSOptions['preprocessorOptions']
   },
+  // customLogger,
   build: {
     target: 'esnext',
     sourcemap: true,
